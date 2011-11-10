da2ec03616921dccd192a47d17b1a9b6098267f1
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2011-11-10 09:22:05 +0000

    增加C++代码框架生成。

--- trunk/Yuki/Src/SchemaManipulator/Properties/Resources.Designer.cs
+++ trunk/Yuki/Src/SchemaManipulator/Properties/Resources.Designer.cs
@@ -67,6 +67,13 @@ namespace Yuki.SchemaManipulator.Properties {
             }
         }
         
+        internal static byte[] Cpp {
+            get {
+                object obj = ResourceManager.GetObject("Cpp", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
         internal static byte[] CSharp {
             get {
                 object obj = ResourceManager.GetObject("CSharp", resourceCulture);
