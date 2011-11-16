c9c2cca96c0c21937b9cf8ce3ec1491e8ed88e3e
Ianus Inferus <r_ex@163.com>
2011-11-16 14:16:15 +0000

    将C++代码生成中的共享指针内存分配改为标准函数。

---
+++ Src/Archieve.cmd
@@ -0,0 +1,15 @@
+@echo off
+@PATH %ProgramFiles%\WinRar;%PATH%
+
+@pushd ..
+@for %%* in (.) do set PackName=%%~n*
+@popd
+
+::@set PackName=PackageName
+
+@call Clear.cmd
+@cd ..
+@del %PackName%Src.rar
+@rar a -av- -m5 -md4096 -tsm -tsc -s -k -t %PackName%Src.rar -x*\.svn -x*\.svn\* -x*.user -x*.suo Src
+@if not exist Versions\ md Versions\
+@copy %PackName%Src.rar Versions\
