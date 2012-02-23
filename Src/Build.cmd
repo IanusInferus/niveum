47b0aa8dd61ae73b996522039ea660fcf7dad2c4
Ianus Inferus <r_ex@163.com>
2012-02-23 16:32:34 +0000

    增加Yuki.Core项目。 增加日期版本号、强名称签名，仿照Firefly项目规范发布。

--- Src/Build.cmd
+++ Src/Build.cmd
@@ -2,4 +2,8 @@ PATH %SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
 
 MSBuild /t:Rebuild /p:Configuration=Release
 
+copy Doc\Readme.*.txt ..\Bin\
+copy Doc\UpdateLog.*.txt ..\Bin\
+copy Doc\License.*.txt ..\Bin\
+
 pause
--- Src/YukiCore/Properties/AssemblyInfo.cs
+++ Src/YukiCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SchemaManipulator")]
+[assembly: AssemblyTitle("Yuki.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yuki.SchemaManipulator")]
+[assembly: AssemblyProduct("Yuki.Core")]
 [assembly: AssemblyCopyright("Copyright(C) F.R.C., under BSD license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("4526109f-577a-46ce-a79d-5b154166775a")]
+[assembly: Guid("7a934aad-ce35-4747-9877-1aac6d369d31")]
 
 // 程序集的版本信息由下面四个值组成:
 //
