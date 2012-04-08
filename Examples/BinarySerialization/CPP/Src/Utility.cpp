e679f5a162252353604f48c696a82c9a232b1b05
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2012-04-08 09:45:35 +0000

    Examples: 修改二进制序列化C++示例项目，使其能在GNU C++ 4.6上运行。

--- Examples/BinarySerialization/CPP/Src/Utility.cpp
+++ Examples/BinarySerialization/CPP/Src/Utility.cpp
@@ -1,135 +1,22 @@
-//==========================================================================
+ï»¿//==========================================================================
 //
 //  File:        Utility.cpp
-//  Location:    Yuki.Examples <Visual C++ 2010>
-//  Description: ¹¤¾ßº¯Êý
-//  Version:     2012.04.07.
+//  Location:    Yuki.Examples <C++ 2011>
+//  Description: å·¥å…·å‡½æ•°
+//  Version:     2012.04.08.
 //  Author:      F.R.C.
 //  Copyright(C) Public Domain
 //
 //==========================================================================
 
-#include <windows.h>
-#include <regex>
-#include <functional>
+#pragma warning (disable : 4345)
+#pragma warning (disable : 4996)
 
 #include "Utility.h"
 
+#include <climits>
+
 using namespace std;
-
-PRIVATE shared_ptr<wstring> DescapeQuota(shared_ptr<wstring> s)
-{
-    if (*s == L"") { return make_shared<wstring>(L""); }
-    if ((s->length() >= 1) && ((*s)[0] == '\"') && ((*s)[s->length() - 1] == '\"'))
-    {
-        return make_shared<wstring>(&((*s)[1]), &((*s)[s->length() - 1]));
-    }
-    else
-    {
-        return s;
-    }
-}
-
-PRIVATE shared_ptr<vector<shared_ptr<wstring>>> SplitCmdLineWithChar(shared_ptr<wstring> CmdLine, shared_ptr<wstring> Pattern, wchar_t c, bool SuppressFirst)
-{
-    auto argv = make_shared<vector<shared_ptr<wstring>>>();
-    bool SuppressedFirst = !SuppressFirst;
-    size_t NextStart = 0;
-    wregex r(*Pattern);
-    wcmatch arg;
-    while ((NextStart < CmdLine->length()) && (regex_search(&(*CmdLine)[NextStart], arg, r)))
-    {
-        if (arg.position() != 0)
-        {
-            size_t Length = arg.position();
-            for (size_t i = 0; i < Length; i += 1)
-            {
-                if ((*CmdLine)[NextStart + i]  != c)
-                {
-                    throw logic_error("InvalidOperation");
-                }
-            }
-        }
-        size_t Start = NextStart + arg.position();
-        NextStart = Start + arg.str().length();
-        if (!SuppressedFirst)
-        {
-            SuppressedFirst = true;
-            continue;
-        }
-        auto m = make_shared<wstring>(*CmdLine, Start, arg.str().length());
-        argv->push_back(DescapeQuota(m));
-    }
-    if (NextStart != CmdLine->length())
-    {
-        size_t Length = CmdLine->length() - NextStart;
-        for (size_t i = 0; i < Length; i += 1)
-        {
-            if ((*CmdLine)[NextStart + i] != c)
-            {
-                throw logic_error("InvalidOperation");
-            }
-        }
-    }
-    return argv;
-}
-
-shared_ptr<CommandLineArguments> GetCmdLine()
-{
-    LPWSTR CmdLine = GetCommandLineW();
-    auto argv = SplitCmdLineWithChar(make_shared<wstring>(CmdLine), make_shared<wstring>(L"(\"[^\"]*\"|([^\" ])+)+"), L' ', true);
-
-    auto Arguments = make_shared<vector<shared_ptr<wstring>>>();
-    auto Options = make_shared<vector<shared_ptr<CommandLineOption>>>();
-
-    for_each(argv->begin(), argv->end(), [&](shared_ptr<wstring> v)
-    {
-        if ((v->length() > 0) && ((*v)[0] == L'/'))
-        {
-            auto OptionLine = make_shared<wstring>(*v, 1, v->length() - 1);
-            shared_ptr<wstring> Name;
-            shared_ptr<wstring> ParameterLine;
-            bool Found = false;
-            size_t Index = 0;
-            for (size_t i = 0; i < OptionLine->length(); i += 1)
-            {
-                if ((*OptionLine)[i] == L':') {
-                    Found = true;
-                    Index = i;
-                    break;
-                }
-            }
-            if (Found)
-            {
-                Name = DescapeQuota(make_shared<wstring>(*OptionLine, 0, Index));
-                ParameterLine = make_shared<wstring>(*OptionLine, Index + 1);
-            }
-            else
-            {
-                Name = DescapeQuota(OptionLine);
-                ParameterLine = make_shared<wstring>(L"");
-            }
-
-            auto Arguments = SplitCmdLineWithChar(ParameterLine, make_shared<wstring>(L"(\"[^\"]*\"|([^\",])+)+"), L',', false);
-
-            auto clo = make_shared<CommandLineOption>();
-            clo->Name = Name;
-            clo->Arguments = Arguments;
-
-            Options->push_back(clo);
-        }
-        else
-        {
-            Arguments->push_back(v);
-        }
-    });
-
-    auto ret = make_shared<CommandLineArguments>();
-    ret->Arguments = Arguments;
-    ret->Options = Options;
-
-    return ret;
-}
 
 bool EqualIgnoreCase(const wstring& l, const wstring& r)
 {
@@ -161,19 +48,19 @@ bool EqualIgnoreCase(shared_ptr<wstring> l, shared_ptr<wstring> r)
 
 shared_ptr<wstring> s2w(shared_ptr<string> s)
 {
-    int n = MultiByteToWideChar(CP_ACP, 0, s->c_str(), s->length(), 0, 0);
-    if(n <= 0) return nullptr;
+    int n = mbstowcs(NULL, s->c_str(), INT_MAX);
+    if (n <= 0) return nullptr;
     auto ws = make_shared<wstring>(n, 0);
-    MultiByteToWideChar(CP_ACP, 0, s->c_str(), s->length(), &(*ws)[0], n);
+    mbstowcs(&(*ws)[0], s->c_str(), n);
     return ws;
 }
 
 shared_ptr<string> w2s(shared_ptr<wstring> ws)
 {
-    int n = WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS, ws->c_str(), ws->length(), 0, 0, "?", NULL);
-    if(n <= 0) return nullptr;
+    int n = wcstombs(NULL, ws->c_str(), INT_MAX);
+    if (n <= 0) return nullptr;
     auto s = make_shared<string>(n, 0);
-    WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS, ws->c_str(), ws->length(), &(*s)[0], n, "?", NULL);
+    wcstombs(&(*s)[0], ws->c_str(), n);
     return s;
 }
 
