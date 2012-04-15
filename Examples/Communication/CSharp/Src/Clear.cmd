38de250412f314b46a3774d9332417d5b2ad498d
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2012-04-15 14:43:38 +0000

    Examples: 增加二进制通讯C#示例项目。

---
+++ Examples/Communication/CSharp/Src/Clear.cmd
@@ -0,0 +1,24 @@
+@for /d %%a in (*) do @(
+  @if exist %%a\obj @(
+    @rd %%a\obj /S /Q
+  )
+)
+@cd..
+@if exist Bin (
+  @cd Bin
+  @for %%a in (*.exe) do @(
+    @del %%~na.pdb /F /Q
+    @del %%~na.xml /F /Q
+  )
+  @del *.vshost.exe /F /Q
+  @del *.manifest /F /Q
+  @del *.CodeAnalysisLog.xml /F /Q
+  @del *.lastcodeanalysissucceeded /F /Q
+  @del Test.* /F /S /Q
+  @cd..
+)
+@cd Src
+attrib -H Communication.suo
+del *.suo /F /Q
+del *.cache /F /Q
+pause
