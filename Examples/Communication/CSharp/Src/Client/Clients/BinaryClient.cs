f1be6c834c9739c7907ea16338f032b196f8f3ee
Ianus Inferus <r_ex@163.com>
2012-04-24 08:25:39 +0000

    Examples; 修改通讯C#示例项目，增加二进制和Json客户端。

---
+++ Examples/Communication/CSharp/Src/Client/Clients/BinaryClient.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using Firefly;
+using Firefly.Streaming;
+using Firefly.TextEncoding;
+using Communication;
+using Communication.Net;
+using Communication.Binary;
+
+namespace Client
+{
+    public sealed class BinaryClient : IBinarySender, IDisposable
+    {
+        private ClientImplementation ci;
+        public BinaryClient<ClientContext> InnerClient { get; private set; }
+        private ClientContext Context;
+
+        private IPEndPoint RemoteEndPoint;
+        private StreamedAsyncSocket sock;
+
+        public BinaryClient(IPEndPoint RemoteEndPoint)
+        {
+            this.RemoteEndPoint = RemoteEndPoint;
+            sock = new StreamedAsyncSocket(new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp));
+            ci = new ClientImplementation();
+            InnerClient = new BinaryClient<ClientContext>(this, ci);
+            Context = new ClientContext();
+            Context.DequeueCallback = InnerClient.DequeueCallback;
+        }
+
+        public void Connect()
+        {
+            sock.InnerSocket.Connect(RemoteEndPoint);
+        }
+
+        void IBinarySender.Send(String CommandName, UInt32 CommandHash, Byte[] Parameters)
+        {
+            var CommandNameBytes = TextEncoding.UTF16.GetBytes(CommandName);
+            Byte[] Bytes;
+            using (var s = Streams.CreateMemoryStream())
+            {
+                s.WriteInt32(CommandNameBytes.Length);
+                s.Write(CommandNameBytes);
+                s.WriteUInt32(CommandHash);
+                s.WriteInt32(Parameters.Length);
+                s.Write(Parameters);
+                s.Position = 0;
+                Bytes = s.Read((int)(s.Length));
+            }
+            sock.InnerSocket.Send(Bytes);
+        }
+
+        private class Command
+        {
+            public String CommandName;
+            public UInt32 CommandHash;
+            public Byte[] Parameters;
+        }
+
+        private class TryShiftResult
+        {
+            public Command Command;
+            public int Position;
+        }
+
+        private class BufferStateMachine
+        {
+            private int State;
+            // 0 初始状态
+            // 1 已读取NameLength
+            // 2 已读取CommandHash
+            // 3 已读取Name
+            // 4 已读取ParametersLength
+
+            private Int32 CommandNameLength;
+            private String CommandName;
+            private UInt32 CommandHash;
+            private Int32 ParametersLength;
+
+            public BufferStateMachine()
+            {
+                State = 0;
+            }
+
+            public TryShiftResult TryShift(Byte[] Buffer, int Position, int Length)
+            {
+                if (State == 0)
+                {
+                    if (Length >= 4)
+                    {
+                        using (var s = new ByteArrayStream(Buffer, Position, Length))
+                        {
+                            CommandNameLength = s.ReadInt32();
+                        }
+                        if (CommandNameLength < 0 || CommandNameLength > 128) { throw new InvalidOperationException(); }
+                        var r = new TryShiftResult { Command = null, Position = Position + 4 };
+                        State = 1;
+                        return r;
+                    }
+                    return null;
+                }
+                else if (State == 1)
+                {
+                    if (Length >= CommandNameLength)
+                    {
+                        using (var s = new ByteArrayStream(Buffer, Position, Length))
+                        {
+                            CommandName = TextEncoding.UTF16.GetString(s.Read(CommandNameLength));
+                        }
+                        var r = new TryShiftResult { Command = null, Position = Position + CommandNameLength };
+                        State = 2;
+                        return r;
+                    }
+                    return null;
+                }
+                else if (State == 2)
+                {
+                    if (Length >= CommandHash)
+                    {
+                        using (var s = new ByteArrayStream(Buffer, Position, Length))
+                        {
+                            CommandHash = s.ReadUInt32();
+                        }
+                        var r = new TryShiftResult { Command = null, Position = Position + 4 };
+                        State = 3;
+                        return r;
+                    }
+                    return null;
+                }
+                if (State == 3)
+                {
+                    if (Length >= 4)
+                    {
+                        using (var s = new ByteArrayStream(Buffer, Position, Length))
+                        {
+                            ParametersLength = s.ReadInt32();
+                        }
+                        if (ParametersLength < 0 || ParametersLength > 8 * 1024) { throw new InvalidOperationException(); }
+                        var r = new TryShiftResult { Command = null, Position = Position + 4 };
+                        State = 4;
+                        return r;
+                    }
+                    return null;
+                }
+                else if (State == 4)
+                {
+                    if (Length >= ParametersLength)
+                    {
+                        Byte[] Parameters;
+                        using (var s = new ByteArrayStream(Buffer, Position, Length))
+                        {
+                            Parameters = s.Read(ParametersLength);
+                        }
+                        var cmd = new Command { CommandName = CommandName, CommandHash = CommandHash, Parameters = Parameters };
+                        var r = new TryShiftResult { Command = cmd, Position = Position + ParametersLength };
+                        CommandNameLength = 0;
+                        CommandName = null;
+                        CommandHash = 0;
+                        ParametersLength = 0;
+                        State = 0;
+                        return r;
+                    }
+                    return null;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        private BufferStateMachine bsm = new BufferStateMachine();
+        private Byte[] Buffer = new Byte[8 * 1024];
+        private int BufferLength = 0;
+
+        private Boolean IsSocketErrorKnown(SocketError se)
+        {
+            if (se == SocketError.ConnectionAborted) { return true; }
+            if (se == SocketError.ConnectionReset) { return true; }
+            if (se == SocketError.Shutdown) { return true; }
+            if (se == SocketError.OperationAborted) { return true; }
+            return false;
+        }
+
+        public void Receive(Action<SocketError> UnknownFaulted)
+        {
+            Action<SocketError> Faulted = se =>
+            {
+                if (!IsSocketErrorKnown(se))
+                {
+                    UnknownFaulted(se);
+                }
+            };
+
+            Action<int> Completed = null;
+            Completed = Count =>
+            {
+                if (Count == 0)
+                {
+                    return;
+                }
+                var FirstPosition = 0;
+                var CheckPosition = BufferLength;
+                BufferLength += Count;
+                while (true)
+                {
+                    var r = bsm.TryShift(Buffer, FirstPosition, BufferLength - FirstPosition);
+                    if (r == null)
+                    {
+                        break;
+                    }
+                    FirstPosition = r.Position;
+
+                    if (r.Command != null)
+                    {
+                        var cmd = r.Command;
+                        InnerClient.HandleResult(Context, cmd.CommandName, cmd.CommandHash, cmd.Parameters);
+                    }
+                }
+                if (FirstPosition > 0)
+                {
+                    var CopyLength = BufferLength - FirstPosition;
+                    for (int i = 0; i < CopyLength; i += 1)
+                    {
+                        Buffer[i] = Buffer[FirstPosition + i];
+                    }
+                    BufferLength = CopyLength;
+                }
+                if (sock == null) { return; }
+                sock.ReceiveAsync(Buffer, BufferLength, Buffer.Length - BufferLength, Completed, Faulted);
+            };
+
+            sock.ReceiveAsync(Buffer, BufferLength, Buffer.Length - BufferLength, Completed, Faulted);
+        }
+
+        public void Close()
+        {
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+        }
+
+        public void Dispose()
+        {
+            sock.Dispose();
+            sock = null;
+        }
+    }
+}
---
+++ Examples/Communication/CSharp/Src/Client/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Firefly;
+using Communication;
+
+namespace Client
+{
+    public enum ApplicationProtocolType
+    {
+        Binary,
+        Json
+    }
+
+    public static class Program
+    {
+        public static int Main(String[] args)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                return MainInner();
+            }
+            else
+            {
+                try
+                {
+                    return MainInner();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ExceptionInfo.GetExceptionInfo(ex));
+                    return -1;
+                }
+            }
+        }
+
+        public static int MainInner()
+        {
+            DisplayTitle();
+
+            var CmdLine = CommandLine.GetCmdLine();
+
+            foreach (var opt in CmdLine.Options)
+            {
+                if ((opt.Name.ToLower() == "?") || (opt.Name.ToLower() == "help"))
+                {
+                    DisplayInfo();
+                    return 0;
+                }
+            }
+
+            var argv = CmdLine.Arguments;
+            if (argv.Length == 3)
+            {
+                Run(new IPEndPoint(IPAddress.Parse(argv[1]), int.Parse(argv[2])), (ApplicationProtocolType)Enum.Parse(typeof(ApplicationProtocolType), argv[0], true));
+            }
+            else if (argv.Length == 1)
+            {
+                Run(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8001), (ApplicationProtocolType)Enum.Parse(typeof(ApplicationProtocolType), argv[0], true));
+            }
+            else if (argv.Length == 0)
+            {
+                Run(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8001), ApplicationProtocolType.Binary);
+            }
+            else
+            {
+                DisplayInfo();
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public static void DisplayTitle()
+        {
+            Console.WriteLine(@"聊天客户端");
+            Console.WriteLine(@"Author:      F.R.C.");
+            Console.WriteLine(@"Copyright(C) Public Domain");
+        }
+
+        public static void DisplayInfo()
+        {
+            Console.WriteLine(@"用法:");
+            Console.WriteLine(@"Client [<Protocol> [<IpAddress> <Port>]]");
+            Console.WriteLine(@"Protocol 通讯协议，可为Binary或Json，默认为Binary");
+            Console.WriteLine(@"IpAddress 服务器IP地址，默认为127.0.0.1");
+            Console.WriteLine(@"Port 服务器端口，默认为8001");
+        }
+
+        public static void Run(IPEndPoint RemoteEndPoint, ApplicationProtocolType ProtocolType)
+        {
+            if (ProtocolType == ApplicationProtocolType.Binary)
+            {
+                using (var bc = new BinaryClient(RemoteEndPoint))
+                {
+                    bc.Connect();
+                    Console.WriteLine("连接成功。");
+                    bc.Receive(se => Console.WriteLine((new SocketException((int)se)).Message));
+                    while (true)
+                    {
+                        var Line = Console.ReadLine();
+                        if (Line == "exit") { break; }
+                        bc.InnerClient.SendMessage(new SendMessageRequest { Content = Line }, (c, r) =>
+                        {
+                            if (r.OnTooLong)
+                            {
+                                Console.WriteLine("消息过长。");
+                            }
+                        });
+                    }
+                    bc.Close();
+                }
+            }
+            else if (ProtocolType == ApplicationProtocolType.Json)
+            {
+                using (var jc = new JsonClient(RemoteEndPoint))
+                {
+                    jc.Connect();
+                    Console.WriteLine("连接成功。");
+                    jc.Receive(se => Console.WriteLine((new SocketException((int)se)).Message));
+                    while (true)
+                    {
+                        var Line = Console.ReadLine();
+                        if (Line == "exit") { break; }
+                        jc.InnerClient.SendMessage(new SendMessageRequest { Content = Line }, (c, r) =>
+                        {
+                            if (r.OnTooLong)
+                            {
+                                Console.WriteLine("消息过长。");
+                            }
+                        });
+                    }
+                    jc.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("协议不能识别：" + ProtocolType.ToString());
+            }
+        }
+    }
+}
