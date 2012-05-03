1a244ebf2a56788f9c59a33b5ea2638e17228e40
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2012-05-03 06:07:16 +0000

    Examples: 修正C++通讯示例中的线程同步问题。 增加C++通讯示例性能测试报告。 Yuki.Core, Yuki.SchemaManipulator, Yuki.Relation, Yuki.RelationSchemaManipulator: 分离出关系数据类型代码生成库和工具。

--- Src/YukiRelation/CSharpDatabase/CodeGenerator.cs
+++ Src/YukiRelation/CSharpDatabase/CodeGenerator.cs
@@ -3,7 +3,7 @@
 //  File:        CodeGenerator.cs
 //  Location:    Yuki.Core <Visual C#>
 //  Description: 对象类型结构C#枚举数据库代码生成器
-//  Version:     2012.04.24.
+//  Version:     2012.05.02.
 //  Copyright(C) F.R.C.
 //
 //==========================================================================
@@ -47,7 +47,7 @@ namespace Yuki.RelationSchema.CSharpDatabase
 
             static Writer()
             {
-                var OriginalTemplateInfo = OS.ObjectSchemaTemplateInfo.FromBinary(Properties.Resources.CSharp);
+                var OriginalTemplateInfo = OS.ObjectSchemaTemplateInfo.FromBinary(OS.Properties.Resources.CSharp);
                 TemplateInfo = OS.ObjectSchemaTemplateInfo.FromBinary(Properties.Resources.CSharpDatabase);
                 TemplateInfo.Keywords = OriginalTemplateInfo.Keywords;
             }
--- Src/YukiRelation/Properties/AssemblyInfo.cs
+++ Src/YukiRelation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Yuki.Core")]
+[assembly: AssemblyTitle("Yuki.Relation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yuki.Core")]
+[assembly: AssemblyProduct("Yuki.Relation")]
 [assembly: AssemblyCopyright("Copyright(C) F.R.C., under BSD license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("7a934aad-ce35-4747-9877-1aac6d369d31")]
+[assembly: Guid("7c98c2d1-36a6-479f-8df2-ab9af362ffee")]
 
 // 程序集的版本信息由下面四个值组成:
 //
