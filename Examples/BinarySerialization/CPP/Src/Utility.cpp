2848852aa821fbc200dde861feb7ce33de353a9f
Ianus Inferus <r_ex@163.com>
2012-05-24 04:05:04 +0000

    Yuki.Core, Examples: 修正C++通讯及示例中字符串转换时当长度为0可能出现的问题。

--- Examples/BinarySerialization/CPP/Src/Utility.cpp
+++ Examples/BinarySerialization/CPP/Src/Utility.cpp
@@ -3,7 +3,7 @@
 //  File:        Utility.cpp
 //  Location:    Yuki.Examples <C++ 2011>
 //  Description: 工具函数
-//  Version:     2012.04.27.
+//  Version:     2012.05.24.
 //  Author:      F.R.C.
 //  Copyright(C) Public Domain
 //
@@ -45,18 +45,20 @@ bool EqualIgnoreCase(shared_ptr<wstring> l, shared_ptr<wstring> r)
 
 shared_ptr<wstring> s2w(shared_ptr<string> s)
 {
-    int n = (int)mbstowcs(NULL, s->c_str(), INT_MAX);
+    int n = static_cast<int>(mbstowcs(NULL, s->c_str(), INT_MAX));
     if (n < 0) { throw logic_error("InvalidOperationException"); }
     auto ws = make_shared<wstring>(n, 0);
+    if (n == 0) { return ws; }
     mbstowcs(&(*ws)[0], s->c_str(), n);
     return ws;
 }
 
 shared_ptr<string> w2s(shared_ptr<wstring> ws)
 {
-    int n = (int)wcstombs(NULL, ws->c_str(), INT_MAX);
+    int n = static_cast<int>(wcstombs(NULL, ws->c_str(), INT_MAX));
     if (n < 0) { throw logic_error("InvalidOperationException"); }
     auto s = make_shared<string>(n, 0);
+    if (n == 0) { return s; }
     wcstombs(&(*s)[0], ws->c_str(), n);
     return s;
 }
