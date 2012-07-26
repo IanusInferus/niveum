8ab1e813c96ef6ded1a395f311b9aa2b39d84856
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2012-07-26 08:22:03 +0000

    Yuki.Core: 去除CommandDef类型。 修正所有的通讯命令Hash计算没有计算命令本身的问题。 Yuki.TcpSendReceive: 将TcpSendReceive工具加入到正式代码，并增加使用通讯Schema和二进制服务器进行Json格式交互的功能。 Examples: 修正通讯示例使用退出命令是没有回显的问题。

---
+++ trunk/Yuki/Src/TcpSendReceive/BaseSystem/LockedVariable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Communication.BaseSystem
+{
+    public class LockedVariable<T>
+    {
+        private T Value;
+        private Object Lockee = new Object();
+        public LockedVariable(T Value)
+        {
+            this.Value = Value;
+        }
+
+        public S Check<S>(Func<T, S> Map)
+        {
+            lock (Lockee)
+            {
+                return Map(Value);
+            }
+        }
+
+        public void DoAction(Action<T> Action)
+        {
+            lock (Lockee)
+            {
+                Action(Value);
+            }
+        }
+
+        public void Update(Func<T, T> Map)
+        {
+            lock (Lockee)
+            {
+                Value = Map(Value);
+            }
+        }
+    }
+}
