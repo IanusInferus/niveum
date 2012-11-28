f339334a15c37f90cc186074f8f077f48dc86418
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2012-11-28 14:02:26 +0000

    Examples: 增加C# PostgreSQL邮件数据库示例。

---
+++ Examples/Database/MailCSharp/Src/PostgreSql/PostgreSqlDataAccessManual.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Npgsql;
+using Database.Database;
+using Boolean = System.Boolean;
+using String = System.String;
+using Type = System.Type;
+using Int = System.Int32;
+using Real = System.Double;
+using Byte = System.Byte;
+
+namespace Database.PostgreSql
+{
+    public partial class PostgreSqlDataAccess : IDataAccess
+    {
+        public List<String> SelectManyMailAttachmentForNameById(Int Id)
+        {
+            var cmd = CreateTextCommand();
+            cmd.CommandText = @"SELECT ""name"" FROM ""mailattachments"" WHERE ""id"" = @id";
+            Add(cmd, "id", Id);
+            var l = new List<String>();
+            using (var dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    var v = GetString(dr, "Name");
+                    l.Add(v);
+                }
+            }
+            return l;
+        }
+    }
+}
