ef2739e2ab530152e6b1347c8d1a12221cef944f
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2012-11-28 11:11:58 +0000

    Examples: 增加C# SQL Server邮件数据库示例。

---
+++ trunk/Yuki/Examples/Database/MailCSharp/Src/SqlServer/SqlServerDataAccessManual.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Database.Database;
+using Boolean = System.Boolean;
+using String = System.String;
+using Type = System.Type;
+using Int = System.Int32;
+using Real = System.Double;
+using Byte = System.Byte;
+
+namespace Database.SqlServer
+{
+    public partial class SqlServerDataAccess : IDataAccess
+    {
+        public List<String> SelectManyMailAttachmentForNameById(Int Id)
+        {
+            var cmd = CreateTextCommand();
+            cmd.CommandText = @"SELECT [Name] FROM [MailAttachments] WHERE [Id] = @Id";
+            Add(cmd, "Id", Id);
+            var l = new List<String>();
+            using (var dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    var v = GetString(dr, "Name");
+                    l.Add(v);
+                }
+            }
+            return l;
+        }
+    }
+}
