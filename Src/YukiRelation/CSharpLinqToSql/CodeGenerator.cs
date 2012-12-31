07a4ef81d5dd3f32b657e4c5b03baabe6b17f369
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2012-12-31 15:19:21 +0000

    Yuki.Relation: 修改数据库代码生成中查询索引为可选类型时的代码不一致的问题。

--- Src/YukiRelation/CSharpLinqToSql/CodeGenerator.cs
+++ Src/YukiRelation/CSharpLinqToSql/CodeGenerator.cs
@@ -3,7 +3,7 @@
 //  File:        CodeGenerator.cs
 //  Location:    Yuki.Relation <Visual C#>
 //  Description: 关系类型结构C# Linq to SQL数据库代码生成器
-//  Version:     2012.12.13.
+//  Version:     2012.12.31.
 //  Copyright(C) F.R.C.
 //
 //==========================================================================
@@ -101,19 +101,6 @@ namespace Yuki.RelationSchema.CSharpLinqToSql
                     }
                 }
                 return l.ToArray();
-            }
-
-            public String GetTypeFriendlyName(TypeSpec Type)
-            {
-                switch (Type._Tag)
-                {
-                    case TypeSpecTag.TypeRef:
-                        return Type.TypeRef.Value;
-                    case TypeSpecTag.List:
-                        return "ListOf" + Type.List.Value;
-                    default:
-                        throw new InvalidOperationException();
-                }
             }
 
             public String GetEnumTypeString(TypeSpec Type)
@@ -639,7 +626,7 @@ namespace Yuki.RelationSchema.CSharpLinqToSql
                 var l = new List<String>();
                 foreach (var c in k.Columns)
                 {
-                    var t = GetTypeFriendlyName(r.Fields.Where(f => f.Name.Equals(c.Name, StringComparison.OrdinalIgnoreCase)).Single().Type);
+                    var t = GetPropertyTypeString(r.Fields.Where(f => f.Name.Equals(c.Name, StringComparison.OrdinalIgnoreCase)).Single());
                     l.Add(String.Format("[[{0}]] [[{1}]]", t, c.Name));
                 }
                 return String.Join(", ", l.ToArray());
