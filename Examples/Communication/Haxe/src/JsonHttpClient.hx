98208d70816f0c8b354bc1d17ccd41e83182c598
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2013-01-25 16:40:12 +0000

    Examples: Haxe通讯示例修正获得的结果没有换行的问题。 将Haxe示例所有代码的Tab转换为空格。

--- Examples/Communication/Haxe/src/JsonHttpClient.hx
+++ Examples/Communication/Haxe/src/JsonHttpClient.hx
@@ -7,80 +7,80 @@ import CommunicationJson;
 
 private class JsonSender implements IJsonSender
 {
-	public var sendObject : Dynamic -> Void;
+    public var sendObject : Dynamic -> Void;
 
-	public function new()
-	{
-	}
-	
-	public function send(commandName : String, commandHash : String, parameters : String) : Void
-	{
-		var jo = {commandName : commandName, commandHash : commandHash, parameters : parameters};
-		sendObject(jo);
-	}
+    public function new()
+    {
+    }
+
+    public function send(commandName : String, commandHash : String, parameters : String) : Void
+    {
+        var jo = {commandName : commandName, commandHash : commandHash, parameters : parameters};
+        sendObject(jo);
+    }
 }
 
 class JsonHttpClient
 {
-	public var InnerClient(get, null) : IApplicationClient;
-	public function get_InnerClient() : IApplicationClient
-	{
-		return jc.GetApplicationClient();
-	}
-	
-	private var jc:JsonSerializationClient;
-	private var sessionId:String;
-	
-	public function new(prefix : String, serviceVirtualPath : String, useJsonp : Bool)
-	{
-		if (!StringTools.endsWith(prefix, "/")) { throw "InvalidOperationException: PrefixNotEndWithSlash: '" + prefix + "'"; }
-		var js = new JsonSender();
-		jc = new JsonSerializationClient(js);
-		sessionId = null;
-		js.sendObject = function(jo : Dynamic)
-		{
-			if (useJsonp)
-			{
-				var url;
-				if (sessionId == null)
-				{
-					url = prefix + serviceVirtualPath + "?callback=?";
-				}
-				else
-				{
-					url = prefix + serviceVirtualPath + "?sessionid=" + StringTools.urlEncode(sessionId) + "&callback=?";
-				}
-				JQueryStatic.getJSON(url, {data : Json.stringify([jo])}, function(r)
-				{
-					var commands : Array<Dynamic> = r.commands;
-					sessionId = r.sessionid;
-					for (c in commands)
-					{
-						jc.handleResult(c.commandName, c.commandHash, c.parameters);
-					}
-				});
-			}
-			else
-			{
-				var url;
-				if (sessionId == null)
-				{
-					url = prefix + serviceVirtualPath;
-				}
-				else
-				{
-					url = prefix + serviceVirtualPath + "?sessionid=" + StringTools.urlEncode(sessionId);
-				}
-				JQueryStatic.post(url, Json.stringify([jo]), function(r)
-				{
-					var commands : Array<Dynamic> = r.commands;
-					sessionId = r.sessionid;
-					for (c in commands)
-					{
-						jc.handleResult(c.commandName, c.commandHash, c.parameters);
-					}
-				});
-			}
-		};
-	}
+    public var InnerClient(get, null) : IApplicationClient;
+    public function get_InnerClient() : IApplicationClient
+    {
+        return jc.GetApplicationClient();
+    }
+
+    private var jc:JsonSerializationClient;
+    private var sessionId:String;
+
+    public function new(prefix : String, serviceVirtualPath : String, useJsonp : Bool)
+    {
+        if (!StringTools.endsWith(prefix, "/")) { throw "InvalidOperationException: PrefixNotEndWithSlash: '" + prefix + "'"; }
+        var js = new JsonSender();
+        jc = new JsonSerializationClient(js);
+        sessionId = null;
+        js.sendObject = function(jo : Dynamic)
+        {
+            if (useJsonp)
+            {
+                var url;
+                if (sessionId == null)
+                {
+                    url = prefix + serviceVirtualPath + "?callback=?";
+                }
+                else
+                {
+                    url = prefix + serviceVirtualPath + "?sessionid=" + StringTools.urlEncode(sessionId) + "&callback=?";
+                }
+                JQueryStatic.getJSON(url, {data : Json.stringify([jo])}, function(r)
+                {
+                    var commands : Array<Dynamic> = r.commands;
+                    sessionId = r.sessionid;
+                    for (c in commands)
+                    {
+                        jc.handleResult(c.commandName, c.commandHash, c.parameters);
+                    }
+                });
+            }
+            else
+            {
+                var url;
+                if (sessionId == null)
+                {
+                    url = prefix + serviceVirtualPath;
+                }
+                else
+                {
+                    url = prefix + serviceVirtualPath + "?sessionid=" + StringTools.urlEncode(sessionId);
+                }
+                JQueryStatic.post(url, Json.stringify([jo]), function(r)
+                {
+                    var commands : Array<Dynamic> = r.commands;
+                    sessionId = r.sessionid;
+                    for (c in commands)
+                    {
+                        jc.handleResult(c.commandName, c.commandHash, c.parameters);
+                    }
+                });
+            }
+        };
+    }
 }
--- Examples/Communication/Haxe/src/Main.hx
+++ Examples/Communication/Haxe/src/Main.hx
@@ -17,66 +17,66 @@ using Lambda;
 
 class Main
 {
-	private	static function	Q(in1 :	Dynamic, ?in2 :	Dynamic) : JQuery
-	{
-		return new JQuery(in1, in2);
-	}
-	private	static function	T(selector : String) : Template
-	{
-		return new Template(Q(selector).text());
-	}
-	
-	static function	main()
-	{
-		var	a =	[1,	2, 3, 4];
-		var	f =	fun(a, b, a	* b);
-		var	h =	f(2, 3);
-		if (h != 6)	{ alert("Error."); };
-		var	b =	a.map(fun(z, z * z)).array();
-		
-		var	d =	BuildDate.getBuildDate();
-		trace("BuildDate: "	+ d.toString() + ",	" +	b.toString());
+    private static function Q(in1 : Dynamic, ?in2 : Dynamic) : JQuery
+    {
+        return new JQuery(in1, in2);
+    }
+    private static function T(selector : String) : Template
+    {
+        return new Template(Q(selector).html());
+    }
 
-		Q(null).ready(function(e)
-		{
-			Q("#button_alert").click(function(e)
-			{
-				alert("Test	Alert Message");
-			});
-			
-			Q("#button_template").click(function(e)
-			{
-				var	users =	[{Id: "zhang3",	Name: "ZHANG San"},	{Id: "li4",	Name: "LI Si"}].list();
-				var	template = T("#tmpl_template");
-				var	text = template.execute({users : users});
-				Q("#tbody_template").append(text);
-			});
+    static function main()
+    {
+        var a = [1, 2, 3, 4];
+        var f = fun(a, b, a * b);
+        var h = f(2, 3);
+        if (h != 6) { alert("Error."); };
+        var b = a.map(fun(z, z * z)).array();
 
-			Q("#button_ajax").click(function(e)
-			{
-				JQueryStatic.getJSON("users.txt", function(r)
-				{
-					var	users =	r;
-					var	template = T("#tmpl_template");
-					var	text = template.execute({users : users});
-					Q("#tbody_ajax").append(text);
-				});
-			});
+        var d = BuildDate.getBuildDate();
+        trace("BuildDate: " + d.toString() + ", " + b.toString());
 
-			var	c =	new	JsonHttpClient("http://localhost/",	"cmd", true);
-			var	jc = c.InnerClient;
-			Q("#button_servertime").click(function(e)
-			{
-				jc.serverTime({}, function(r)
-				{
-					switch (r)
-					{
-						case success(time):
-							var	template = T("#tmpl_paragraph");
-							Q("#div_servertime").append(template.execute({Content :	time}));
-					}
-				});
-			});
-		});
-	}
+        Q(null).ready(function(e)
+        {
+            Q("#button_alert").click(function(e)
+            {
+                alert("Test Alert Message");
+            });
+
+            Q("#button_template").click(function(e)
+            {
+                var users = [{Id: "zhang3", Name: "ZHANG San"}, {Id: "li4", Name: "LI Si"}].list();
+                var template = T("#tmpl_template");
+                var text = template.execute({users : users});
+                Q("#tbody_template").append(text);
+            });
+
+            Q("#button_ajax").click(function(e)
+            {
+                JQueryStatic.getJSON("users.txt", function(r)
+                {
+                    var users = r;
+                    var template = T("#tmpl_template");
+                    var text = template.execute({users : users});
+                    Q("#tbody_ajax").append(text);
+                });
+            });
+
+            var c = new JsonHttpClient("http://localhost/", "cmd", true);
+            var jc = c.InnerClient;
+            Q("#button_servertime").click(function(e)
+            {
+                jc.serverTime({}, function(r)
+                {
+                    switch (r)
+                    {
+                        case success(time):
+                            var template = T("#tmpl_paragraph");
+                            Q("#div_servertime").append(template.execute({Content : time}));
+                    }
+                });
+            });
+        });
+    }
 }
