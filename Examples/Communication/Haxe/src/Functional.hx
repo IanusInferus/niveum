503dd47cbeaf0ae002adffebdcde35c7639fe3e6
Ianus Inferus <r_ex@163.com>
2013-01-25 16:40:12 +0000

    Examples: Haxe通讯示例修正获得的结果没有换行的问题。 将Haxe示例所有代码的Tab转换为空格。

--- Examples/Communication/Haxe/src/Functional.hx
+++ Examples/Communication/Haxe/src/Functional.hx
@@ -4,61 +4,61 @@ using Lambda;
 
 class Functional
 {
-	@:macro	public static function fun(e : Array<Expr>)	: Expr
-	{
-		if (e.length <=	0) { Context.error("At least one argument is required.", Context.currentPos());	}
-		var	body = e[e.length -	1];
-		var	args = e.slice(0, e.length - 1);
-		var	argExprs = new Array<FunctionArg>();
-		
-		for	(x in args)
-		{
-			var	xName :	String = null;
-			switch (x.expr)
-			{
-				case EConst(c):
-					switch (c)
-					{
-						case CIdent(s):
-							xName =	s;
-						default:
-					}
-				default:
-			}
-			if (xName == null) { Context.error("Identifier expected.", x.pos); }
-			argExprs.push({name	: xName, opt : false, type : null, value : null});
-		}
-		
-		var	Return : Expr =	{expr :	ExprDef.EReturn(body), pos : body.pos};
-		return {expr : ExprDef.EFunction(null, {args : argExprs, ret : null, expr :	Return,	params : []}), pos : Context.currentPos()};
-	}
+    @:macro public static function fun(e : Array<Expr>) : Expr
+    {
+        if (e.length <= 0) { Context.error("At least one argument is required.", Context.currentPos()); }
+        var body = e[e.length - 1];
+        var args = e.slice(0, e.length - 1);
+        var argExprs = new Array<FunctionArg>();
 
-	@:macro	public static function sub(e : Array<Expr>)	: Expr
-	{
-		if (e.length <=	0) { Context.error("At least one argument is required.", Context.currentPos());	}
-		var	body = e[e.length -	1];
-		var	args = e.slice(0, e.length - 1);
-		var	argExprs = new Array<FunctionArg>();
-		
-		for	(x in args)
-		{
-			var	xName :	String = null;
-			switch (x.expr)
-			{
-				case EConst(c):
-					switch (c)
-					{
-						case CIdent(s):
-							xName =	s;
-						default:
-					}
-				default:
-			}
-			if (xName == null) { Context.error("Identifier expected.", x.pos); }
-			argExprs.push({name	: xName, opt : false, type : null, value : null});
-		}
-		
-		var	returnType = ComplexType.TPath({pack : [], name	: "Void", params : [], sub : null});
-		return {expr : ExprDef.EFunction(null, {args : argExprs, ret : returnType, expr	: body,	params : []}), pos : Context.currentPos()};
-	}
+        for (x in args)
+        {
+            var xName : String = null;
+            switch (x.expr)
+            {
+                case EConst(c):
+                    switch (c)
+                    {
+                        case CIdent(s):
+                            xName = s;
+                        default:
+                    }
+                default:
+            }
+            if (xName == null) { Context.error("Identifier expected.", x.pos); }
+            argExprs.push({name : xName, opt : false, type : null, value : null});
+        }
+
+        var Return : Expr = {expr : ExprDef.EReturn(body), pos : body.pos};
+        return {expr : ExprDef.EFunction(null, {args : argExprs, ret : null, expr : Return, params : []}), pos : Context.currentPos()};
+    }
+
+    @:macro public static function sub(e : Array<Expr>) : Expr
+    {
+        if (e.length <= 0) { Context.error("At least one argument is required.", Context.currentPos()); }
+        var body = e[e.length - 1];
+        var args = e.slice(0, e.length - 1);
+        var argExprs = new Array<FunctionArg>();
+
+        for (x in args)
+        {
+            var xName : String = null;
+            switch (x.expr)
+            {
+                case EConst(c):
+                    switch (c)
+                    {
+                        case CIdent(s):
+                            xName = s;
+                        default:
+                    }
+                default:
+            }
+            if (xName == null) { Context.error("Identifier expected.", x.pos); }
+            argExprs.push({name : xName, opt : false, type : null, value : null});
+        }
+
+        var returnType = ComplexType.TPath({pack : [], name : "Void", params : [], sub : null});
+        return {expr : ExprDef.EFunction(null, {args : argExprs, ret : returnType, expr : body, params : []}), pos : Context.currentPos()};
+    }
 }
