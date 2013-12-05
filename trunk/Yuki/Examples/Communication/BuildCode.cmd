d6af0111ba4bd86129ce81a9004900c612f2db95
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2013-12-05 12:49:08 +0000

    Examples: 修改通讯代码tree文件和生成脚本。

--- trunk/Yuki/Examples/Communication/BuildCode.cmd
+++ trunk/Yuki/Examples/Communication/BuildCode.cmd
@@ -3,23 +3,23 @@
 :: C#
 @if not exist CSharp\Src\Server\Generated @md CSharp\Src\Server\Generated
 SchemaManipulator.exe /loadtype:Schema\Common /t2cs:CSharp\Src\Server\Generated\Common.cs,"",true
-SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /t2cs:CSharp\Src\Server\Generated\Communication.cs,Communication,true
-SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /t2csb:CSharp\Src\Server\Generated\CommunicationBinary.cs,Communication.Binary,true
-SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /t2csj:CSharp\Src\Server\Generated\CommunicationJson.cs,Communication.Json
+SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2cs:CSharp\Src\Server\Generated\Communication.cs,Communication,true
+SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2csb:CSharp\Src\Server\Generated\CommunicationBinary.cs,Communication.Binary,true
+SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2csj:CSharp\Src\Server\Generated\CommunicationJson.cs,Communication.Json
 SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:CSharp\Src\Server\Configuration.Schema.tree /t2cs:CSharp\Src\Server\Configuration.cs,Server
 @if not exist CSharp\Src\Client\Generated @md CSharp\Src\Client\Generated
 SchemaManipulator.exe /loadtype:Schema\Common /t2cs:CSharp\Src\Client\Generated\Common.cs,"",false
-SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /t2cs:CSharp\Src\Client\Generated\Communication.cs,Communication,false
-SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /t2csb:CSharp\Src\Client\Generated\CommunicationBinary.cs,Communication.Binary,false
-SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /t2csj:CSharp\Src\Client\Generated\CommunicationJson.cs,Communication.Json
+SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2cs:CSharp\Src\Client\Generated\Communication.cs,Communication,false
+SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2csb:CSharp\Src\Client\Generated\CommunicationBinary.cs,Communication.Binary,false
+SchemaManipulator.exe /loadtyperef:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2csj:CSharp\Src\Client\Generated\CommunicationJson.cs,Communication.Json
 
 :: C++2011
 @if not exist CPP\Src\Server @md CPP\Src\Server
 @if not exist CPP\Src\Client @md CPP\Src\Client
-SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /t2cpp:CPP\Src\Server\Communication.h,Communication
-SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /t2cppb:CPP\Src\Server\CommunicationBinary.h,Communication.Binary
-copy CPP\Src\Server\Communication.h CPP\Src\Client\ /Y
-copy CPP\Src\Server\CommunicationBinary.h CPP\Src\Client\ /Y
+SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2cpp:CPP\Src\Server\Communication.h,Communication
+SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /loadtype:Schema\Compatibility /t2cppb:CPP\Src\Server\CommunicationBinary.h,Communication.Binary
+SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /t2cpp:CPP\Src\Client\Communication.h,Communication
+SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /t2cppb:CPP\Src\Client\CommunicationBinary.h,Communication.Binary
 
 :: ActionScript
 @if not exist ActionScript\src\communication @md ActionScript\src\communication
