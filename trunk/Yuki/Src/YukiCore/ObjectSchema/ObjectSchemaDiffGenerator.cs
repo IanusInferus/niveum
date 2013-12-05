a9db87b27db530fc342951982b2ed176c5562476
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2013-12-05 12:47:15 +0000

    Yuki.Core, Yuki.SchemaManipulator: 增加对象数据结构比较。

---
+++ trunk/Yuki/Src/YukiCore/ObjectSchema/ObjectSchemaDiffGenerator.cs
@@ -0,0 +1,109 @@
+﻿//==========================================================================
+//
+//  File:        ObjectSchemaDiffGenerator.cs
+//  Location:    Yuki.Core <Visual C#>
+//  Description: 对象类型结构差异生成器
+//  Version:     2013.12.05.
+//  Copyright(C) F.R.C.
+//
+//==========================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yuki.ObjectSchema
+{
+    public class ObjectSchemaDiffResult
+    {
+        public Schema Patch;
+        public Schema Revert;
+    }
+
+    public sealed class ObjectSchemaDiffGenerator
+    {
+        public ObjectSchemaDiffGenerator()
+        {
+        }
+
+        public ObjectSchemaDiffResult Generate(Schema Left, Schema Right)
+        {
+            foreach (var t in Left.Types)
+            {
+                if (t.Version() != "") { throw new InvalidOperationException(String.Format("VersionedTypeIsNotAllowed: {0}", t.Name())); }
+            }
+            foreach (var t in Right.Types)
+            {
+                if (t.Version() != "") { throw new InvalidOperationException(String.Format("VersionedTypeIsNotAllowed: {0}", t.Name())); }
+            }
+
+            Func<Schema, Func<TypeDef, Schema>> GetGen = s =>
+            {
+                var g = s.GetSubSchemaGenerator();
+                return t => g(new TypeDef[] { t }, new TypeSpec[] { });
+            };
+
+            var LeftGen = GetGen(Left);
+            var RightGen = GetGen(Right);
+            var LeftTypeRefBinaries = Left.TypeRefs.ToDictionary(t => t.Name(), t => LeftGen(t).GetUnifiedBinaryRepresentation(), StringComparer.OrdinalIgnoreCase);
+            var RightTypeRefBinaries = Right.TypeRefs.ToDictionary(t => t.Name(), t => RightGen(t).GetUnifiedBinaryRepresentation(), StringComparer.OrdinalIgnoreCase);
+            var CommonTypeRefs = LeftTypeRefBinaries.Keys.Intersect(RightTypeRefBinaries.Keys, StringComparer.OrdinalIgnoreCase).ToList();
+            if ((LeftTypeRefBinaries.Count > CommonTypeRefs.Count) || (RightTypeRefBinaries.Count > CommonTypeRefs.Count))
+            {
+                var LeftTypeRefExceptRightTypeRef = LeftTypeRefBinaries.Keys.Except(CommonTypeRefs, StringComparer.OrdinalIgnoreCase).ToList();
+                var RightTypeRefExceptLeftTypeRef = RightTypeRefBinaries.Keys.Except(CommonTypeRefs, StringComparer.OrdinalIgnoreCase).ToList();
+                throw new InvalidOperationException(String.Format("TypeRefNotMatch: {0} - {1}", String.Join(" ", LeftTypeRefExceptRightTypeRef), String.Join(" ", RightTypeRefExceptLeftTypeRef)));
+            }
+
+            var ChangedTypeRefs = new List<String>();
+            foreach (var Name in LeftTypeRefBinaries.Keys)
+            {
+                var LeftBinary = LeftTypeRefBinaries[Name];
+                var RightBinary = RightTypeRefBinaries[Name];
+                if (!LeftBinary.SequenceEqual(RightBinary))
+                {
+                    ChangedTypeRefs.Add(Name);
+                }
+            }
+            if (ChangedTypeRefs.Count > 0)
+            {
+                throw new InvalidOperationException(String.Format("TypeRefChanged: {0}", String.Join(" ", ChangedTypeRefs)));
+            }
+
+            var LeftTypeBinaries = Left.Types.ToDictionary(t => t.Name(), t => LeftGen(t).GetUnifiedBinaryRepresentation(), StringComparer.OrdinalIgnoreCase);
+            var RightTypeBinaries = Right.Types.ToDictionary(t => t.Name(), t => RightGen(t).GetUnifiedBinaryRepresentation(), StringComparer.OrdinalIgnoreCase);
+            var CommonTypes = new HashSet<String>(StringComparer.OrdinalIgnoreCase);
+            foreach (var Name in LeftTypeBinaries.Keys.Intersect(RightTypeBinaries.Keys))
+            {
+                var LeftBinary = LeftTypeBinaries[Name];
+                var RightBinary = RightTypeBinaries[Name];
+
+                if (LeftBinary.SequenceEqual(RightBinary))
+                {
+                    CommonTypes.Add(Name);
+                }
+            }
+
+            var Patch = new Schema
+            {
+                Types = Right.Types.Where(t => !CommonTypes.Contains(t.Name())).ToArray(),
+                TypeRefs = Right.TypeRefs.Concat(Right.Types.Where(t => CommonTypes.Contains(t.Name()))).ToArray(),
+                Imports = Right.Imports.Except(Left.Imports, StringComparer.OrdinalIgnoreCase).ToArray(),
+                TypePaths = Right.TypePaths.Where(tp => !CommonTypes.Contains(tp.Name)).ToArray()
+            };
+            var Revert = new Schema
+            {
+                Types = Left.Types.Where(t => !CommonTypes.Contains(t.Name())).ToArray(),
+                TypeRefs = Left.TypeRefs.Concat(Left.Types.Where(t => CommonTypes.Contains(t.Name()))).ToArray(),
+                Imports = Left.Imports.Except(Right.Imports, StringComparer.OrdinalIgnoreCase).ToArray(),
+                TypePaths = Right.TypePaths.Where(tp => !CommonTypes.Contains(tp.Name)).ToArray()
+            };
+
+            return new ObjectSchemaDiffResult
+            {
+                Patch = Patch,
+                Revert = Revert
+            };
+        }
+    }
+}
---
+++ trunk/Yuki/Src/YukiCore/ObjectSchema/ObjectSchemaWriter.cs
@@ -0,0 +1,217 @@
+﻿//==========================================================================
+//
+//  File:        ObjectSchemaWriter.cs
+//  Location:    Yuki.Core <Visual C#>
+//  Description: 对象类型结构写入器
+//  Version:     2013.12.05.
+//  Copyright(C) F.R.C.
+//
+//==========================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Firefly;
+using Firefly.Streaming;
+using Firefly.TextEncoding;
+using Firefly.Texting;
+using Firefly.Texting.TreeFormat;
+using Syntax = Firefly.Texting.TreeFormat.Syntax;
+
+namespace Yuki.ObjectSchema
+{
+    public class ObjectSchemaWriter
+    {
+        public ObjectSchemaWriter()
+        {
+        }
+
+        public String Write(TypeDef[] Types)
+        {
+            var f = WriteToForest(Types);
+
+            String Compiled;
+            using (var ms = Streams.CreateMemoryStream())
+            {
+                using (var tw = Txt.CreateTextWriter(ms.Partialize(0, Int64.MaxValue, 0).AsNewWriting(), TextEncoding.UTF8))
+                {
+                    var sw = new TreeFormatSyntaxWriter(tw);
+                    sw.Write(f);
+                }
+                ms.Position = 0;
+                using (var tr = Txt.CreateTextReader(ms.Partialize(0, ms.Length).AsNewReading(), TextEncoding.UTF8))
+                {
+                    Compiled = tr.ReadToEnd();
+                }
+            }
+            return Compiled;
+        }
+
+        private Syntax.Forest WriteToForest(TypeDef[] Types)
+        {
+            var MultiNodesList = new List<Syntax.MultiNodes>();
+            foreach (var t in Types)
+            {
+                var LineTokens = new List<String[]>();
+                if (t.OnPrimitive)
+                {
+                    var p = t.Primitive;
+                    LineTokens.AddRange(p.GenericParameters.Select(v => WriteToTokens(v, true)));
+                }
+                else if (t.OnAlias)
+                {
+                    var a = t.Alias;
+                    LineTokens.Add(new String[] { GetTypeString(a.Type) });
+                }
+                else if (t.OnRecord)
+                {
+                    var r = t.Record;
+                    LineTokens.AddRange(r.GenericParameters.Select(v => WriteToTokens(v, true)));
+                    LineTokens.AddRange(r.Fields.Select(v => WriteToTokens(v, false)));
+                }
+                else if (t.OnTaggedUnion)
+                {
+                    var tu = t.TaggedUnion;
+                    LineTokens.AddRange(tu.GenericParameters.Select(v => WriteToTokens(v, true)));
+                    LineTokens.AddRange(tu.Alternatives.Select(v => WriteToTokens(v, false)));
+                }
+                else if (t.OnEnum)
+                {
+                    var e = t.Enum;
+                    LineTokens.AddRange(e.Literals.Select(l => WriteToTokens(l)));
+                }
+                else if (t.OnClientCommand)
+                {
+                    var cc = t.ClientCommand;
+                    LineTokens.AddRange(cc.OutParameters.Select(v => WriteToTokens(v, false)));
+                    LineTokens.Add(new String[] { ">" });
+                    LineTokens.AddRange(cc.InParameters.Select(v => WriteToTokens(v, false)));
+                }
+                else if (t.OnServerCommand)
+                {
+                    var sc = t.ServerCommand;
+                    LineTokens.AddRange(sc.OutParameters.Select(v => WriteToTokens(v, false)));
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+                var LineLiterals = LineTokens.Select(l => l.Select(Token => TreeFormatLiteralWriter.GetLiteral(Token, true, false).SingleLine).ToArray()).ToArray();
+                var NumColumn = 0;
+                foreach (var l in LineLiterals)
+                {
+                    NumColumn = Math.Max(NumColumn, l.Length);
+                }
+                var ColumnWidths = new List<int>();
+                for (int k = 0; k <= NumColumn - 2; k += 1)
+                {
+                    int Width = 28;
+                    foreach (var l in LineLiterals)
+                    {
+                        if (k < l.Length)
+                        {
+                            var Column = l[k];
+                            Width = Math.Max(Width, CalculateCharWidth(Column).CeilToMultipleOf(4) + 4);
+                        }
+                    }
+                    ColumnWidths.Add(Width);
+                }
+                var Lines = new List<Syntax.TextLine>();
+                foreach (var l in LineTokens)
+                {
+                    var Line = new List<String>();
+                    for (int k = 0; k < l.Length; k += 1)
+                    {
+                        var Column = l[k];
+                        if ((k != l.Length - 1) && (k < ColumnWidths.Count))
+                        {
+                            var Width = ColumnWidths[k];
+                            Line.Add(Column + new String(' ', Width - CalculateCharWidth(Column)));
+                        }
+                        else
+                        {
+                            Line.Add(Column);
+                        }
+                    }
+                    var LineText = String.Join("", Line.ToArray());
+                    Lines.Add(new Syntax.TextLine { Text = LineText, Range = new Syntax.TextRange { Start = new Syntax.TextPosition { Row = 1, Column = 1, CharIndex = 0 }, End = new Syntax.TextPosition { Row = 1, Column = 1 + LineText.Length, CharIndex = LineText.Length } } });
+                }
+                var fn = WriteToFunctionNodes(t._Tag.ToString(), t.VersionedName(), t.Description(), Lines.ToArray());
+                MultiNodesList.Add(Syntax.MultiNodes.CreateFunctionNodes(fn));
+            }
+            return new Syntax.Forest { MultiNodesList = MultiNodesList.ToArray() };
+        }
+
+        private Syntax.FunctionNodes WriteToFunctionNodes(String Directive, String Name, String Description, Syntax.TextLine[] Lines)
+        {
+            var Parameters = new List<Syntax.Token>();
+            Parameters.Add(Syntax.Token.CreateSingleLineLiteral(Name));
+            if (Description != "")
+            {
+                Parameters.Add(Syntax.Token.CreateSingleLineLiteral(Description));
+            }
+            var fn = new Syntax.FunctionNodes
+            {
+                FunctionDirective = new Syntax.FunctionDirective { Text = Directive },
+                Parameters = Parameters.ToArray(),
+                SingleLineComment = Opt<Syntax.SingleLineComment>.Empty,
+                Content = new Syntax.FunctionContent { IndentLevel = 0, Lines = Lines },
+                EndDirective = Opt<Syntax.EndDirective>.Empty
+            };
+            return fn;
+        }
+
+        private String[] WriteToTokens(VariableDef v, bool IsGenericParameter)
+        {
+            var l = new List<String>();
+            if (IsGenericParameter)
+            {
+                l.Add("'" + v.Name);
+            }
+            else
+            {
+                l.Add(v.Name);
+            }
+            l.Add(GetTypeString(v.Type));
+            if (v.Description != "")
+            {
+                l.Add(v.Description);
+            }
+            return l.ToArray();
+        }
+
+        private String[] WriteToTokens(LiteralDef Literal)
+        {
+            var l = new List<String>();
+            l.Add(Literal.Name);
+            l.Add(Literal.Value.ToInvariantString());
+            if (Literal.Description != "")
+            {
+                l.Add(Literal.Description);
+            }
+            return l.ToArray();
+        }
+
+        private String GetTypeString(TypeSpec Type)
+        {
+            switch (Type._Tag)
+            {
+                case TypeSpecTag.TypeRef:
+                    return Type.TypeRef.VersionedName();
+                case TypeSpecTag.GenericParameterRef:
+                    return "'" + Type.GenericParameterRef.Value;
+                case TypeSpecTag.Tuple:
+                    return "Tuple<" + String.Join(", ", Type.Tuple.Types.Select(t => GetTypeString(t)).ToArray()) + ">";
+                case TypeSpecTag.GenericTypeSpec:
+                    return GetTypeString(Type.GenericTypeSpec.TypeSpec) + "<" + String.Join(", ", Type.GenericTypeSpec.GenericParameterValues.Select(p => GetTypeString(p.TypeSpec)).ToArray()) + ">";
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private int CalculateCharWidth(String s)
+        {
+            return s.ToUTF32().Select(c => c.IsHalfWidth() ? 1 : 2).Sum();
+        }
+    }
+}
