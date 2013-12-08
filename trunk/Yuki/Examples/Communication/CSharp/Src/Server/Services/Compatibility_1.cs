1e882eb211ecb70fe812e44d7e76f4b450386a40
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2013-12-08 15:13:13 +0000

    Examples: 修改C#通讯示例，增加通讯兼容代码。

---
+++ trunk/Yuki/Examples/Communication/CSharp/Src/Server/Services/Compatibility_1.cs
@@ -0,0 +1,47 @@
+﻿//==========================================================================
+//
+//  Notice:      This file is varmatically generated.
+//               Please don't modify this file.
+//
+//==========================================================================
+
+//Reference:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Communication;
+using Firefly;
+using Firefly.TextEncoding;
+
+namespace Server.Services
+{
+    public partial class ServerImplementation
+    {
+        public SendMessageRequest SendMessageAt1RequestToHead(SendMessageAt1Request o)
+        {
+            var ho = new SendMessageRequest();
+            ho.Content = (o.Title != "" ? o.Title + "\r\n" : "") + String.Join("\r\n", o.Lines);
+            return ho;
+        }
+        public SendMessageAt1Reply SendMessageAt1ReplyFromHead(SendMessageReply ho)
+        {
+            if (ho.OnSuccess)
+            {
+                return SendMessageAt1Reply.CreateSuccess();
+            }
+            if (ho.OnTooLong)
+            {
+                return SendMessageAt1Reply.CreateLinesTooLong();
+            }
+            throw new InvalidOperationException();
+        }
+        public MessageReceivedAt1Event MessageReceivedAt1EventFromHead(MessageReceivedEvent ho)
+        {
+            var o = new MessageReceivedAt1Event();
+            o.Title = "<Title>";
+            o.Lines = ho.Content.UnifyNewLineToLf().Split('\n').ToList();
+            return o;
+        }
+    }
+}
