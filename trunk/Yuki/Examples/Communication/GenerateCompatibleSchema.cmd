10deecc977616bbaea11150731c944c476ffa261
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2013-12-27 16:03:25 +0000

    Examples: 调整通讯示例，改进版本比较脚本，使得其支持增量比较。

--- trunk/Yuki/Examples/Communication/GenerateCompatibleSchema.cmd
+++
@@ -1,9 +0,0 @@
-@PATH ..\..\Bin;%PATH%
-
-@for /D %%a in (PreviousSchemas\*) do (
-  SchemaManipulator.exe /loadtype:"%%a\Common" /loadtype:"%%a\Communication" /save:"%%a\Communication.tree"
-  SchemaManipulator.exe /loadtype:Schema\Common /loadtype:Schema\Communication /gencom:"%%a\Communication.tree","Schema\Compatibility\%%~na.tree","%%~na"
-  del "%%a\Communication.tree"
-)
-
-@pause
