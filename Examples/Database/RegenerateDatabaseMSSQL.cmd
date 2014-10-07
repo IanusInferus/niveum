25e10ab81bab0befed6880d09e87d36bcf21589c
Ianus Inferus <r_ex@163.com>
2014-10-07 12:03:37 +0000

    Examples: 更新C#数据库代码生成的性能测试结果。

--- Examples/Database/RegenerateDatabaseMSSQL.cmd
+++ Examples/Database/RegenerateDatabaseMSSQL.cmd
@@ -1,6 +1,6 @@
 @PATH ..\..\Bin;%PATH%
 
-DatabaseRegenerator.exe /loadtyperef:CommonSchema /loadtype:MailSchema /connect:"Data Source=(LocalDB)\v11.0;Integrated Security=True" /database:Mail /regenmssql:MailData
-DatabaseRegenerator.exe /loadtyperef:CommonSchema /loadtype:TestSchema /connect:"Data Source=(LocalDB)\v11.0;Integrated Security=True" /database:Test /regenmssql:TestData
+DatabaseRegenerator.exe /loadtyperef:CommonSchema /loadtype:MailSchema /connect:"Data Source=.;Integrated Security=True" /database:Mail /regenmssql:MailData
+DatabaseRegenerator.exe /loadtyperef:CommonSchema /loadtype:TestSchema /connect:"Data Source=.;Integrated Security=True" /database:Test /regenmssql:TestData
 
 @pause
