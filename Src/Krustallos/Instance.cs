57f7094fd4f6b625e5b4c1e52d67cde26c270ec7
Ianus Inferus <r_ex@163.com>
2014-10-19 12:31:42 +0000

    Krustallos: 增加Krustallos内存事务数据库代码。

---
+++ Src/Krustallos/Instance.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Krustallos
+{
+    /// <summary>
+    /// Krustallos 内存事务数据库
+    /// 支持只读事务多版本镜像访问(MVCC)
+    /// 支持读写事务两阶段提交(2PL)
+    /// 支持悲观并发控制(Pessimistic Concurrency Control)
+    /// 写需要锁定整个事务写入的所有表
+    /// 不支持自增字段
+    /// </summary>
+    public class Instance
+    {
+        private Object ReaderAllocateLockee = new Object();
+        private Version CurrentReaderVersion = new Version(0);
+        private ImmutableSortedDictionary<Version, int> ReaderCounts = new ImmutableSortedDictionary<Version, int>();
+        private Object WriterAllocateLockee = new Object();
+        private Version CurrentWriterVersion = new Version(0);
+        private ImmutableSortedDictionary<Version, Unit> WriterExists = new ImmutableSortedDictionary<Version, Unit>();
+        private Object ToBeRemovedLockee = new Object();
+        private ImmutableSortedDictionary<Version, HashSet<IVersionedStore>> ToBeRemoved = new ImmutableSortedDictionary<Version, HashSet<IVersionedStore>>();
+        public Version TakeReaderVersion()
+        {
+            lock (ReaderAllocateLockee)
+            {
+                if (ReaderCounts.ContainsKey(CurrentReaderVersion))
+                {
+                    ReaderCounts = ReaderCounts.SetItem(CurrentReaderVersion, ReaderCounts.TryGetValue(CurrentReaderVersion).Value + 1);
+                }
+                else
+                {
+                    ReaderCounts = ReaderCounts.Add(CurrentReaderVersion, 1);
+                }
+                return CurrentReaderVersion;
+            }
+        }
+        public void ReturnReaderVersion(Version CommittedVersion)
+        {
+            var MinReaderVersion = Optional<Version>.Empty;
+            lock (ReaderAllocateLockee)
+            {
+                if (ReaderCounts.ContainsKey(CommittedVersion))
+                {
+                    var Count = ReaderCounts.TryGetValue(CommittedVersion).Value;
+                    Count -= 1;
+                    if (Count <= 0)
+                    {
+                        ReaderCounts = ReaderCounts.Remove(CommittedVersion);
+                        var oPair = ReaderCounts.TryGetMinPair();
+                        if (oPair.OnHasValue)
+                        {
+                            MinReaderVersion = oPair.Value.Key;
+                        }
+                        else
+                        {
+                            MinReaderVersion = CurrentReaderVersion;
+                        }
+                    }
+                    else
+                    {
+                        ReaderCounts = ReaderCounts.SetItem(CommittedVersion, Count);
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            if (MinReaderVersion.OnHasValue)
+            {
+                RemoveOldVersions(MinReaderVersion.Value);
+            }
+        }
+
+        private void RemoveOldVersions(Krustallos.Version Version)
+        {
+            var Stores = new HashSet<IVersionedStore>();
+            lock (ToBeRemovedLockee)
+            {
+                foreach (var p in ToBeRemoved.Range(Optional<Version>.Empty, Version))
+                {
+                    foreach (var s in p.Value)
+                    {
+                        if (!Stores.Contains(s))
+                        {
+                            Stores.Add(s);
+                        }
+                    }
+                    ToBeRemoved = ToBeRemoved.Remove(p.Key);
+                }
+            }
+            foreach (var s in Stores)
+            {
+                s.RemovePreviousVersions(Version);
+            }
+        }
+        public Version CreateWriterVersion()
+        {
+            lock (WriterAllocateLockee)
+            {
+                CurrentWriterVersion += 1;
+                WriterExists = WriterExists.Add(CurrentWriterVersion, default(Unit));
+                return CurrentWriterVersion;
+            }
+        }
+        public void RevertWriterVersion(Version CommittingVersion)
+        {
+            Version MaxCommittedVersion;
+            lock (WriterAllocateLockee)
+            {
+                WriterExists = WriterExists.Remove(CurrentWriterVersion);
+                var oPair = WriterExists.TryGetMinPair();
+                if (oPair.OnHasValue)
+                {
+                    MaxCommittedVersion = oPair.Value.Key - 1;
+                }
+                else
+                {
+                    MaxCommittedVersion = CurrentWriterVersion;
+                }
+            }
+            lock (ReaderAllocateLockee)
+            {
+                CurrentReaderVersion = MaxCommittedVersion;
+            }
+        }
+        public void CommitWriterVersion(Version CommittingVersion, HashSet<IVersionedStore> ToBeRemoved)
+        {
+            lock (ToBeRemovedLockee)
+            {
+                this.ToBeRemoved = this.ToBeRemoved.Add(CommittingVersion, ToBeRemoved);
+            }
+            Version MaxCommittedVersion;
+            lock (WriterAllocateLockee)
+            {
+                WriterExists = WriterExists.Remove(CurrentWriterVersion);
+                var oPair = WriterExists.TryGetMinPair();
+                if (oPair.OnHasValue)
+                {
+                    MaxCommittedVersion = oPair.Value.Key - 1;
+                }
+                else
+                {
+                    MaxCommittedVersion = CurrentWriterVersion;
+                }
+            }
+            var MinReaderVersion = Optional<Version>.Empty;
+            lock (ReaderAllocateLockee)
+            {
+                CurrentReaderVersion = MaxCommittedVersion;
+                var oPair = ReaderCounts.TryGetMinPair();
+                if (oPair.OnHasValue)
+                {
+                    MinReaderVersion = oPair.Value.Key;
+                }
+                else
+                {
+                    MinReaderVersion = CurrentReaderVersion;
+                }
+            }
+            if (MinReaderVersion.OnHasValue)
+            {
+                RemoveOldVersions(MinReaderVersion.Value);
+            }
+        }
+    }
+}
---
+++ Src/Krustallos/VersionedStore.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Krustallos
+{
+    public interface IVersionedStore
+    {
+        void RemoveVersion(Version v);
+        void RemovePreviousVersions(Version v);
+
+    }
+    public class VersionedStore<TKey, TValue> : IVersionedStore
+    {
+        private ImmutableSortedDictionary<Version, ImmutableSortedDictionary<TKey, TValue>> Versions = new ImmutableSortedDictionary<Version, ImmutableSortedDictionary<TKey, TValue>>((Left, Right) => -Left.CompareTo(Right));
+        private Func<TKey, TKey, int> Compare;
+
+        private class DefaultComparer
+        {
+            public int Compare(TKey x, TKey y)
+            {
+                return ((IComparable<TKey>)(x)).CompareTo(y);
+            }
+        }
+        private class ReversedDefaultComparer
+        {
+            public int Compare(TKey x, TKey y)
+            {
+                return -((IComparable<TKey>)(x)).CompareTo(y);
+            }
+        }
+
+        public VersionedStore()
+        {
+            this.Compare = (new DefaultComparer()).Compare;
+        }
+        public VersionedStore(bool IsReversed)
+        {
+            if (IsReversed)
+            {
+                this.Compare = (new ReversedDefaultComparer()).Compare;
+            }
+            else
+            {
+                this.Compare = (new DefaultComparer()).Compare;
+            }
+        }
+        public VersionedStore(Func<TKey, TKey, int> ConcurrentCompare)
+        {
+            this.Compare = ConcurrentCompare;
+        }
+
+        public ImmutableSortedDictionary<TKey, TValue> GetVersionContent(Version v)
+        {
+            var Versions = Interlocked.CompareExchange(ref this.Versions, null, null);
+            foreach (var Pair in Versions.Range(v, Optional<Version>.Empty))
+            {
+                return Pair.Value;
+            }
+            return new ImmutableSortedDictionary<TKey, TValue>(Compare);
+        }
+
+        public Optional<KeyValuePair<Version, ImmutableSortedDictionary<TKey, TValue>>> TryGetLastVersion()
+        {
+            var Versions = Interlocked.CompareExchange(ref this.Versions, null, null);
+            return Versions.TryGetPairByIndex(0);
+        }
+        public ImmutableSortedDictionary<TKey, TValue> GetLastVersionContent()
+        {
+            var Versions = Interlocked.CompareExchange(ref this.Versions, null, null);
+            var oPair = Versions.TryGetPairByIndex(0);
+            if (oPair.OnHasValue)
+            {
+                return oPair.Value.Value;
+            }
+            else
+            {
+                return new ImmutableSortedDictionary<TKey, TValue>(Compare);
+            }
+        }
+
+        public void PutVersion(Version v, ImmutableSortedDictionary<TKey, TValue> Content)
+        {
+            while (true)
+            {
+                var Versions = Interlocked.CompareExchange(ref this.Versions, null, null);
+                var NewVersion = (Versions.ContainsKey(v) ? Versions.Remove(v) : Versions).Add(v, Content);
+                if (Interlocked.CompareExchange(ref this.Versions, NewVersion, Versions) == Versions)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void RemoveVersion(Version v)
+        {
+            while (true)
+            {
+                var Versions = Interlocked.CompareExchange(ref this.Versions, null, null);
+                if (!Versions.ContainsKey(v))
+                {
+                    break;
+                }
+                if (Interlocked.CompareExchange(ref this.Versions, Versions.Remove(v), Versions) == Versions)
+                {
+                    break;
+                }
+            }
+        }
+        public void RemovePreviousVersions(Version v)
+        {
+            while (true)
+            {
+                var Versions = Interlocked.CompareExchange(ref this.Versions, null, null);
+                var NewVersions = Versions;
+                foreach (var p in Versions.Range(v, Optional<Version>.Empty).Skip(1))
+                {
+                    if (NewVersions.Count > 1)
+                    {
+                        NewVersions = NewVersions.Remove(p.Key);
+                        break;
+                    }
+                }
+                if (Interlocked.CompareExchange(ref this.Versions, NewVersions, Versions) == Versions)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
