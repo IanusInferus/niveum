0e08bc233c22e419eff1b87c782fcfe34c5192eb
Ianus Inferus <r_ex@163.com>
2014-10-27 07:09:22 +0000

    Examples: 修改C#通讯示例，去除部分数组创建，优化性能。

--- Examples/Communication/CSharp/Src/Client/Clients/UdpClient.cs
+++ Examples/Communication/CSharp/Src/Client/Clients/UdpClient.cs
@@ -334,7 +334,7 @@ namespace Client
                         var Verification = 0;
                         if (SecureContext != null)
                         {
-                            var Key = SecureContext.ServerToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4).ToArray()));
+                            var Key = SecureContext.ServerToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4)));
                             var HMACBytes = Cryptography.HMACSHA1(Key, Buffer).Take(4).ToArray();
                             Verification = HMACBytes[0] | ((Int32)(HMACBytes[1]) << 8) | ((Int32)(HMACBytes[2]) << 16) | ((Int32)(HMACBytes[3]) << 24);
                         }
@@ -470,7 +470,7 @@ namespace Client
                         var Verification = 0;
                         if (SecureContext != null)
                         {
-                            var Key = SecureContext.ServerToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4).ToArray()));
+                            var Key = SecureContext.ServerToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4)));
                             var HMACBytes = Cryptography.HMACSHA1(Key, Buffer).Take(4).ToArray();
                             Verification = HMACBytes[0] | ((Int32)(HMACBytes[1]) << 8) | ((Int32)(HMACBytes[2]) << 16) | ((Int32)(HMACBytes[3]) << 24);
                         }
@@ -600,7 +600,7 @@ namespace Client
 
                         if (IsEncrypted)
                         {
-                            var Key = SecureContext.ClientToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4).ToArray()));
+                            var Key = SecureContext.ClientToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4)));
                             var HMACBytes = Cryptography.HMACSHA1(Key, Buffer).Take(4).ToArray();
                             var HMAC = HMACBytes[0] | ((Int32)(HMACBytes[1]) << 8) | ((Int32)(HMACBytes[2]) << 16) | ((Int32)(HMACBytes[3]) << 24);
                             if (HMAC != Verification) { return; }
