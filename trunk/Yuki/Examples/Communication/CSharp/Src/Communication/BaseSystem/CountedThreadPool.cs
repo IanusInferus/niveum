8420c6cbe8ae11208484ab9b3d267e3ad924b322
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2014-11-02 20:03:35 +0000

    Examples: 修改C#通讯示例，将线程池实现改为使用阻塞队列。

--- trunk/Yuki/Examples/Communication/CSharp/Src/Communication/BaseSystem/CountedThreadPool.cs
+++ trunk/Yuki/Examples/Communication/CSharp/Src/Communication/BaseSystem/CountedThreadPool.cs
@@ -9,42 +9,31 @@ namespace BaseSystem
     public class CountedThreadPool : IDisposable
     {
         private CancellationTokenSource TokenSource;
-        private ManualResetEvent ThreadPoolExit;
-        private AutoResetEvent WorkItemAdded;
-        private ConcurrentQueue<Action> WorkItems;
-        private WaitHandle[] WaitHandles;
+        private BlockingCollection<Action> WorkItems;
         private Thread[] Threads;
 
         public CountedThreadPool(String Name, int ThreadCount)
         {
             TokenSource = new CancellationTokenSource();
-            ThreadPoolExit = new ManualResetEvent(false);
-            WorkItemAdded = new AutoResetEvent(false);
-            WorkItems = new ConcurrentQueue<Action>();
+            WorkItems = new BlockingCollection<Action>();
 
             var Token = TokenSource.Token;
-            WaitHandles = new WaitHandle[] { ThreadPoolExit, WorkItemAdded };
             Threads = Enumerable.Range(0, ThreadCount).Select((i, t) => new Thread(() =>
             {
                 Thread.CurrentThread.Name = Name + "[" + i.ToString() + "]";
                 while (true)
                 {
-                    var Result = WaitHandle.WaitAny(WaitHandles);
-                    if (Result == 0) { break; }
-                    int EmptyCount = 0;
-                    while (true)
+                    if (Token.IsCancellationRequested) { return; }
+                    Action a;
+                    try
                     {
-                        if (Token.IsCancellationRequested) { return; }
-                        Action a;
-                        while (WorkItems.TryDequeue(out a))
-                        {
-                            EmptyCount = 0;
-                            a();
-                        }
-                        EmptyCount += 1;
-                        if (EmptyCount > 128) { break; }
-                        Thread.SpinWait(1 + EmptyCount);
+                        a = WorkItems.Take(Token);
                     }
+                    catch (OperationCanceledException)
+                    {
+                        return;
+                    }
+                    a();
                 }
             })).ToArray();
             foreach (var t in Threads)
@@ -55,21 +44,18 @@ namespace BaseSystem
 
         public void QueueUserWorkItem(Action WorkItem)
         {
-            WorkItems.Enqueue(WorkItem);
-            WorkItemAdded.Set();
+            WorkItems.Add(WorkItem);
         }
 
         public void Dispose()
         {
             TokenSource.Cancel();
-            ThreadPoolExit.Set();
             foreach (var t in Threads)
             {
                 t.Join();
             }
 
-            WorkItemAdded.Dispose();
-            ThreadPoolExit.Dispose();
+            WorkItems.Dispose();
             TokenSource.Dispose();
         }
     }
