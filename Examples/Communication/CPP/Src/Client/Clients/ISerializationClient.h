bfad45c56df28c76e9f680691cea95144c5d08d7
Ianus Inferus <r_ex@163.com>
2014-11-02 09:59:38 +0000

    Examples: 修改C#、C++通讯示例，去除冗余代码。

--- Examples/Communication/CPP/Src/Client/Clients/ISerializationClient.h
+++ Examples/Communication/CPP/Src/Client/Clients/ISerializationClient.h
@@ -14,7 +14,6 @@ namespace Client
         virtual ~IBinarySerializationClientAdapter() {}
 
         virtual std::uint64_t Hash() = 0;
-        virtual void DequeueCallback(std::wstring CommandName) = 0;
         virtual void HandleResult(std::wstring CommandName, std::uint32_t CommandHash, std::shared_ptr<std::vector<std::uint8_t>> Parameters) = 0;
         std::function<void(std::wstring CommandName, std::uint32_t CommandHash, std::shared_ptr<std::vector<std::uint8_t>> Parameters)> ClientEvent;
     };
