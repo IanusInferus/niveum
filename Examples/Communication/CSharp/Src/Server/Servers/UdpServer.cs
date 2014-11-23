3258f29adb6f4418fd912aa9826b3cdac360e450
Ianus Inferus <r_ex@163.com>
2014-11-23 10:24:46 +0000

    Examples: 修改C#通讯示例，增加HMAC-SHA1标准实现。

--- Examples/Communication/CSharp/Src/Server/Servers/UdpServer.cs
+++ Examples/Communication/CSharp/Src/Server/Servers/UdpServer.cs
@@ -485,7 +485,7 @@ namespace Server
                                             if (IsEncrypted)
                                             {
                                                 var Key = SecureContext.ServerToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4)));
-                                                var HMACBytes = Cryptography.HMACSHA1(Key, Buffer).Take(4).ToArray();
+                                                var HMACBytes = Cryptography.HMACSHA1Simple(Key, Buffer).Take(4).ToArray();
                                                 var HMAC = HMACBytes[0] | ((Int32)(HMACBytes[1]) << 8) | ((Int32)(HMACBytes[2]) << 16) | ((Int32)(HMACBytes[3]) << 24);
                                                 if (HMAC != Verification) { return; }
                                             }
