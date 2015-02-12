272dc12bb9b2439083efc070071169b4f566314f
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2015-02-12 09:48:57 +0000

    Examples: 增加C++表达式示例。

---
+++ Examples/Expression/CPP/Src/ExpressionCalculator.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include "ExpressionSchema.h"
+
+#include <memory>
+#include <string>
+#include <functional>
+#include <vector>
+#include <unordered_map>
+#include <boost/any.hpp>
+
+namespace Yuki
+{
+    namespace Expression
+    {
+        int pow(int Left, int Right);
+        double pow(double Left, double Right);
+        int mod(int v, int m);
+        int div(int Left, int Right);
+        int round(double v);
+        int floor(double v);
+        int ceil(double v);
+        double round(double v, int NumFractionDigit);
+        double floor(double v, int NumFractionDigit);
+        double ceil(double v, int NumFractionDigit);
+        int min(int v1, int v2);
+        int max(int v1, int v2);
+        int clamp(int v, int LowerBound, int UpperBound);
+        double min(double v1, double v2);
+        double max(double v1, double v2);
+        double clamp(double v, double LowerBound, double UpperBound);
+        int abs(int v);
+        double rand();
+        int rand(int LowerBound, int UpperBoundExclusive);
+        double rand(double LowerBound, double UpperBoundExclusive);
+
+        class ExpressionParameterContext
+        {
+        public:
+            std::unordered_map<std::wstring, boost::any> Parameters;
+        };
+
+        class ExpressionParameterTypeProvider
+        {
+        public:
+            std::unordered_map<std::wstring, Yuki::ExpressionSchema::PrimitiveType> Parameters;
+
+            ExpressionParameterTypeProvider()
+            {
+            }
+
+            ExpressionParameterTypeProvider(std::shared_ptr<std::vector<std::shared_ptr<Yuki::ExpressionSchema::VariableDef>>> ParameterDefs)
+            {
+                for (int k = 0; k < static_cast<int>(ParameterDefs->size()); k += 1)
+                {
+                    auto p = (*ParameterDefs)[k];
+                    Parameters[p->Name] = p->Type;
+                }
+            }
+        };
+
+        class ExpressionCalculator
+        {
+        public:
+            ExpressionCalculator()
+            {
+            }
+
+            template <typename T>
+            std::function<T(ExpressionParameterContext &)> BuildExpression(ExpressionParameterTypeProvider &eptp, std::shared_ptr<Yuki::ExpressionSchema::Expr> e)
+            {
+                auto f = BuildExpr(eptp, e);
+                return boost::any_cast<std::function<T(ExpressionParameterContext &)>>(f);
+            }
+
+        private:
+            boost::any BuildExpr(ExpressionParameterTypeProvider &eptp, std::shared_ptr<Yuki::ExpressionSchema::Expr> e);
+        };
+    }
+}
---
+++ Examples/Expression/CPP/Src/Streams.h
@@ -0,0 +1,90 @@
+﻿//==========================================================================
+//
+//  File:        Streams.h
+//  Location:    Yuki.Examples <C++ 2011>
+//  Description: 文件流
+//  Version:     2015.02.11.
+//  Author:      F.R.C.
+//  Copyright(C) Public Domain
+//
+//==========================================================================
+
+#pragma once
+
+#include "ExpressionSchema.h"
+#include "ExpressionSchemaBinary.h"
+#include "Utility.h"
+
+#include <cstdio>
+
+namespace Yuki
+{
+    namespace ExpressionSchema
+    {
+        using namespace std;
+
+        class ReadableStream : public IReadableStream
+        {
+        private:
+            FILE *f;
+        public:
+            ReadableStream(String Path)
+            {
+                f = fopen(w2s(Path).c_str(), "rb");
+                if (f == NULL)
+                {
+                    throw runtime_error("IOException");
+                }
+            }
+            ~ReadableStream()
+            {
+                fclose(f);
+            }
+
+            uint8_t ReadByte()
+            {
+                uint8_t b = 0;
+                if (fread(&b, 1, 1, f) != 1)
+                {
+                    throw runtime_error("IOException");
+                }
+                return b;
+            }
+            shared_ptr<vector<uint8_t>> ReadBytes(size_t Size)
+            {
+                throw runtime_error("NotSupported");
+            }
+        };
+
+        class WritableStream : public IWritableStream
+        {
+        private:
+            FILE *f;
+        public:
+            WritableStream(String Path)
+            {
+                f = fopen(w2s(Path).c_str(), "wb");
+                if (f == NULL)
+                {
+                    throw runtime_error("IOException");
+                }
+            }
+            ~WritableStream()
+            {
+                fclose(f);
+            }
+
+            virtual void WriteByte(uint8_t b)
+            {
+                if (fwrite(&b, 1, 1, f) != 1)
+                {
+                    throw runtime_error("IOException");
+                }
+            }
+            virtual void WriteBytes(shared_ptr<vector<uint8_t>> l)
+            {
+                throw runtime_error("NotSupported");
+            }
+        };
+    }
+}
