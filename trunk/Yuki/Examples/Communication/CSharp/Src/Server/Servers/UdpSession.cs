9287209b57d7dd17bdddea7b3fbd2e8592f97de4
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2015-09-08 08:02:12 +0000

    Examples: 修正C#和C++通讯示例中UDP传输层包加密Token不兼容的问题。

--- trunk/Yuki/Examples/Communication/CSharp/Src/Server/Servers/UdpSession.cs
+++ trunk/Yuki/Examples/Communication/CSharp/Src/Server/Servers/UdpSession.cs
@@ -377,7 +377,7 @@ namespace Server
                     var Verification = 0;
                     if (SecureContext != null)
                     {
-                        var Key = SecureContext.ClientToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4)));
+                        var Key = SecureContext.ServerToken.Concat(Cryptography.SHA1(Buffer.Skip(4).Take(4)));
                         var HMACBytes = Cryptography.HMACSHA1Simple(Key, Buffer).Take(4).ToArray();
                         Verification = HMACBytes[0] | ((Int32)(HMACBytes[1]) << 8) | ((Int32)(HMACBytes[2]) << 16) | ((Int32)(HMACBytes[3]) << 24);
                     }
