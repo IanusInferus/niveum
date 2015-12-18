9cfe2a9172c4133c0c0746cc2b799d0bdf3e853b
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2015-12-18 07:34:35 +0000

    Examples: C++通讯示例增加带毫秒的时间函数。

--- Examples/Communication/CPP/Src/Server/BaseSystem/Times.h
+++ Examples/Communication/CPP/Src/Server/BaseSystem/Times.h
@@ -9,4 +9,10 @@ std::wstring DateTimeUtcToString(std::chrono::system_clock::time_point Time);
 // parse ISO 8601 string 'yyyy-MM-ddTHH:mm:ssZ'
 std::chrono::system_clock::time_point StringToDateTimeUtc(std::wstring s);
 
+// time to ISO 8601 string 'yyyy-MM-ddTHH:mm:ss.fffZ'
+std::wstring DateTimeUtcWithMillisecondsToString(std::chrono::system_clock::time_point Time);
+
+// parse ISO 8601 string 'yyyy-MM-ddTHH:mm:ss.fffZ'
+std::chrono::system_clock::time_point StringToDateTimeUtcWithMillisecond(std::wstring s);
+
 std::chrono::system_clock::time_point UtcNow();
