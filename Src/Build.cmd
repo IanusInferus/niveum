82923739b342c10ea61c9420e12fcfaa17c82dbf
Ianus Inferus <r_ex@163.com>
2016-05-20 15:00:00 +0000

    *: 修正MSBuild路径。

--- Src/Build.cmd
+++ Src/Build.cmd
@@ -1,4 +1,4 @@
-PATH %SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
+PATH %ProgramFiles(x86)%\MSBuild\14.0\Bin;%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
 
 MSBuild /t:Rebuild /p:Configuration=Release
 
