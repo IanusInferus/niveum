aac64b90ac92a0a9ac5c00aeb6cac33af1b982d6
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2016-05-27 15:53:07 +0000

    *: 更新Firefly库。 Nivea: 完善表达式解析转换，修正词法。

--- Src/Nivea/Template/Syntax/TypeParser.cs
+++ Src/Nivea/Template/Syntax/TypeParser.cs
@@ -3,7 +3,7 @@
 //  File:        TypeParser.cs
 //  Location:    Nivea <Visual C#>
 //  Description: 类型词法解析器
-//  Version:     2016.05.23.
+//  Version:     2016.05.27.
 //  Copyright(C) F.R.C.
 //
 //==========================================================================
@@ -39,12 +39,28 @@ namespace Nivea.Template.Syntax
 
         public static TypeSpec ParseTypeSpec(String TypeString, Action<Object, int, int> Mark, Func<int, Exception> InvalidCharExceptionGenerator)
         {
-            return ParseTypeSpec(TypeString, 0, Mark, InvalidCharExceptionGenerator);
+            int InvalidCharIndex;
+            var ov = TryParseTypeSpec(TypeString, 0, Mark, out InvalidCharIndex);
+            if (ov.OnNotHasValue)
+            {
+                throw InvalidCharExceptionGenerator(InvalidCharIndex);
+            }
+            return ov.Value;
+        }
+        public static Optional<TypeSpec> TryParseTypeSpec(String TypeString, Action<Object, int, int> Mark, out int InvalidCharIndex)
+        {
+            return TryParseTypeSpec(TypeString, 0, Mark, out InvalidCharIndex);
         }
 
-        private static TypeSpec ParseTypeSpec(String TypeString, int Offset, Action<Object, int, int> Mark, Func<int, Exception> InvalidCharExceptionGenerator)
+        private static Optional<TypeSpec> TryParseTypeSpec(String TypeString, int Offset, Action<Object, int, int> Mark, out int InvalidCharIndex)
         {
-            var tsl = ParseTypeSpecLiteral(TypeString, Offset, InvalidCharExceptionGenerator);
+            InvalidCharIndex = Offset;
+            var otsl = TryParseTypeSpecLiteral(TypeString, false, Offset, out InvalidCharIndex);
+            if (otsl.OnNotHasValue)
+            {
+                return Optional<TypeSpec>.Empty;
+            }
+            var tsl = otsl.Value;
             var TypeNamePair = tsl.TypeName;
             var TypeName = TypeNamePair.Key;
             var Parameters = tsl.Parameters;
@@ -54,7 +70,8 @@ namespace Nivea.Template.Syntax
             {
                 if (Char.IsWhiteSpace(c))
                 {
-                    throw InvalidCharExceptionGenerator(Offset + Index);
+                    InvalidCharIndex = Offset + Index;
+                    return Optional<TypeSpec>.Empty;
                 }
                 Index += 1;
             }
@@ -74,18 +91,98 @@ namespace Nivea.Template.Syntax
             if (Parameters.Count == 0)
             {
                 Mark(t, Offset + TypeNamePair.Value, Offset + TypeNamePair.Value + TypeName.Length);
-                return t;
             }
-
-            if (String.Equals(TypeName, "Tuple", StringComparison.OrdinalIgnoreCase))
+            else if (String.Equals(TypeName, "Tuple", StringComparison.OrdinalIgnoreCase))
             {
-                t = TypeSpec.CreateTuple(Parameters.Select(p => ParseTypeSpec(p.Key, p.Value, Mark, InvalidCharExceptionGenerator)).ToList());
+                var l = new List<TypeSpec>();
+                foreach (var p in Parameters)
+                {
+                    var ov = TryParseTypeSpec(p.Key, p.Value, Mark, out InvalidCharIndex);
+                    if (ov.OnNotHasValue)
+                    {
+                        return Optional<TypeSpec>.Empty;
+                    }
+                    l.Add(ov.Value);
+                }
+                t = TypeSpec.CreateTuple(l);
                 Mark(t, Offset, Offset + TypeString.Length);
-                return t;
+            }
+            else
+            {
+                var l = new List<TypeSpec>();
+                foreach (var p in Parameters)
+                {
+                    var ov = TryParseTypeSpec(p.Key, p.Value, Mark, out InvalidCharIndex);
+                    if (ov.OnNotHasValue)
+                    {
+                        return Optional<TypeSpec>.Empty;
+                    }
+                    l.Add(ov.Value);
+                }
+                t = TypeSpec.CreateGenericTypeSpec(new GenericTypeSpec { TypeSpec = t, ParameterValues = l });
+                Mark(t, Offset, Offset + TypeString.Length);
             }
 
-            t = TypeSpec.CreateGenericTypeSpec(new GenericTypeSpec { TypeSpec = t, ParameterValues = Parameters.Select(p => ParseTypeSpec(p.Key, p.Value, Mark, InvalidCharExceptionGenerator)).ToList() });
-            Mark(t, Offset, Offset + TypeString.Length);
+            if (tsl.Child.OnHasValue)
+            {
+                var MemberList = new Queue<KeyValuePair<TypeSpec, int>>();
+                MemberList.Enqueue(new KeyValuePair<TypeSpec, int>(t, Offset + tsl.Child.Value.Value - 1));
+                while (tsl.Child.OnHasValue)
+                {
+                    var ChildString = tsl.Child.Value.Key;
+                    var ChildOffset = Offset + tsl.Child.Value.Value;
+                    otsl = TryParseTypeSpecLiteral(ChildString, true, ChildOffset, out InvalidCharIndex);
+                    if (otsl.OnNotHasValue)
+                    {
+                        return Optional<TypeSpec>.Empty;
+                    }
+                    tsl = otsl.Value;
+                    TypeNamePair = tsl.TypeName;
+                    TypeName = TypeNamePair.Key;
+                    Parameters = tsl.Parameters;
+
+                    if (TypeName.StartsWith("'"))
+                    {
+                        return Optional<TypeSpec>.Empty;
+                    }
+                    else
+                    {
+                        var Ref = ParseTypeRef(TypeName);
+                        t = TypeSpec.CreateTypeRef(Ref);
+                    }
+
+                    if (Parameters.Count == 0)
+                    {
+                        Mark(t, ChildOffset + TypeNamePair.Value, ChildOffset + TypeNamePair.Value + TypeName.Length);
+                    }
+                    else
+                    {
+                        var l = new List<TypeSpec>();
+                        foreach (var p in Parameters)
+                        {
+                            var ov = TryParseTypeSpec(p.Key, p.Value, Mark, out InvalidCharIndex);
+                            if (ov.OnNotHasValue)
+                            {
+                                return Optional<TypeSpec>.Empty;
+                            }
+                            l.Add(ov.Value);
+                        }
+                        t = TypeSpec.CreateGenericTypeSpec(new GenericTypeSpec { TypeSpec = t, ParameterValues = l });
+                        Mark(t, ChildOffset, ChildOffset + ChildString.Length);
+                    }
+                    MemberList.Enqueue(new KeyValuePair<TypeSpec, int>(t, tsl.Child.OnHasValue ? ChildOffset + tsl.Child.Value.Value - 1 : Offset + TypeString.Length));
+                }
+
+                var pp = MemberList.Dequeue();
+                t = pp.Key;
+                while (MemberList.Count > 0)
+                {
+                    var tNext = MemberList.Peek();
+                    t = TypeSpec.CreateMember(new TypeMemberSpec { Parent = t, Child = tNext.Key });
+                    Mark(t, Offset, tNext.Value);
+                    pp = MemberList.Dequeue();
+                }
+            }
             return t;
         }
 
@@ -93,8 +190,9 @@ namespace Nivea.Template.Syntax
         {
             public KeyValuePair<String, int> TypeName;
             public List<KeyValuePair<String, int>> Parameters;
+            public Optional<KeyValuePair<String, int>> Child;
         }
-        private static TypeSpecLiteral ParseTypeSpecLiteral(String TypeString, int Offset, Func<int, Exception> InvalidCharExceptionGenerator)
+        private static Optional<TypeSpecLiteral> TryParseTypeSpecLiteral(String TypeString, Boolean IsInChild, int Offset, out int InvalidCharIndex)
         {
             //State 0       开始
             //State 1+n     内部
@@ -105,6 +203,7 @@ namespace Nivea.Template.Syntax
             //    EndOfString -> 结束
             //    < -> State 1
             //    > -> 失败
+            //    . -> 如果IsInChild，则后方为子结点，结束，否则加入到TypeNameChars，前进
             //    _ -> 加入到TypeNameChars，前进
             //
             //State 1
@@ -116,6 +215,12 @@ namespace Nivea.Template.Syntax
             //
             //State 2
             //    EndOfString -> 结束
+            //    Space -> State 3，前进
+            //    . -> 后方为子结点，结束
+            //    _ -> 失败
+            //
+            //State 3
+            //    EndOfString -> 结束
             //    Space -> 前进
             //    _ -> 失败
 
@@ -123,6 +228,8 @@ namespace Nivea.Template.Syntax
             var ParamStartIndex = 0;
             var ParamChars = new List<Char>();
             var ParameterStrings = new List<KeyValuePair<String, int>>();
+            var HasChild = false;
+            var ChildStartIndex = 0;
 
             var Index = 0;
             Action Proceed = () => Index += 1;
@@ -144,7 +251,22 @@ namespace Nivea.Template.Syntax
                     }
                     else if (c == ">")
                     {
-                        throw InvalidCharExceptionGenerator(Offset + Index);
+                        InvalidCharIndex = Offset + Index;
+                        return Optional<TypeSpecLiteral>.Empty;
+                    }
+                    else if (c == ".")
+                    {
+                        if (IsInChild)
+                        {
+                            HasChild = true;
+                            ChildStartIndex = Index + 1;
+                            break;
+                        }
+                        else
+                        {
+                            TypeNameChars.AddRange(c);
+                            Proceed();
+                        }
                     }
                     else
                     {
@@ -209,11 +331,33 @@ namespace Nivea.Template.Syntax
                     var c = PeekChar();
                     if (c == " ")
                     {
+                        State = 3;
+                        Proceed();
+                    }
+                    else if (c == ".")
+                    {
+                        HasChild = true;
+                        ChildStartIndex = Index + 1;
+                        break;
+                    }
+                    else
+                    {
+                        InvalidCharIndex = Offset + Index;
+                        return Optional<TypeSpecLiteral>.Empty;
+                    }
+                }
+                else if (State == 3)
+                {
+                    if (EndOfString()) { break; }
+                    var c = PeekChar();
+                    if (c == " ")
+                    {
                         Proceed();
                     }
                     else
                     {
-                        throw InvalidCharExceptionGenerator(Offset + Index);
+                        InvalidCharIndex = Offset + Index;
+                        return Optional<TypeSpecLiteral>.Empty;
                     }
                 }
                 else
@@ -230,8 +374,14 @@ namespace Nivea.Template.Syntax
             }
 
             var TypeName = new String(TypeNameChars.ToArray());
+            var Child = Optional<KeyValuePair<String, int>>.Empty;
+            if (HasChild)
+            {
+                Child = new KeyValuePair<String, int>(TypeString.Substring(ChildStartIndex), ChildStartIndex);
+            }
 
-            return new TypeSpecLiteral { TypeName = new KeyValuePair<String, int>(TypeName.Trim(' '), TypeName.TakeWhile(cc => cc == ' ').Count()), Parameters = ParameterStrings };
+            InvalidCharIndex = Offset;
+            return new TypeSpecLiteral { TypeName = new KeyValuePair<String, int>(TypeName.Trim(' '), TypeName.TakeWhile(cc => cc == ' ').Count()), Parameters = ParameterStrings, Child = Child };
         }
     }
 }
