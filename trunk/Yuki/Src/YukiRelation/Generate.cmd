5e5f6b60ede98ca6f57bda80ad5701c0803e07e1
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2016-10-11 16:35:08 +0000

    Yuki.Relation: 修正编译。

--- trunk/Yuki/Src/YukiRelation/Generate.cmd
+++ trunk/Yuki/Src/YukiRelation/Generate.cmd
@@ -1,4 +1,5 @@
 :: ..\..\Bin\Nivea.exe /cs:.,Generated
 ..\..\Bin\Nivea.exe /cs:RelationSchema,Generated\RelationSchema
+..\..\Bin\Nivea.exe /cs:RelationSchemaDiff,Generated\RelationSchemaDiff
 
 @pause
