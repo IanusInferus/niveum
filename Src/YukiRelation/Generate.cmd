4eb03eb879a3816b0b46701ec927c64ec88989b8
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2016-10-12 02:01:43 +0000

    Yuki.Relation: 修正编译。

--- Src/YukiRelation/Generate.cmd
+++ Src/YukiRelation/Generate.cmd
@@ -1,5 +1,6 @@
 :: ..\..\Bin\Nivea.exe /cs:.,Generated
 ..\..\Bin\Nivea.exe /cs:RelationSchema,Generated\RelationSchema
+..\..\Bin\Nivea.exe /cs:RelationValue,Generated\RelationValue
 ..\..\Bin\Nivea.exe /cs:RelationSchemaDiff,Generated\RelationSchemaDiff
 
 @pause
