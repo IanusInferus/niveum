b9dc921a567d8d5386c900b2c80c89a7d1ea6ef2
Ianus Inferus <r_ex@163.com>
2016-10-16 14:34:40 +0000

    Yuki.Expression: 增加新版C#表达式计算二进制程序集加载代码生成，移除旧版。

--- Src/YukiCore/Generate.cmd
+++ Src/YukiCore/Generate.cmd
@@ -1,4 +1,3 @@
-:: ..\..\Bin\Nivea.exe /cs:.,Generated
 ..\..\Bin\Nivea.exe /cs:Common,Generated\Common
 ..\..\Bin\Nivea.exe /cs:ObjectSchema,Generated\ObjectSchema
 ..\..\Bin\Nivea.exe /cs:Generator,Generated\Generator
