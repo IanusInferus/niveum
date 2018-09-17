b7470f0f2c791b77b988cb1cb036f93c3231781f
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2018-09-17 16:00:21 +0000

    Niveum.Json: 增加JSON解析库，完成词法分析。 Doc: 增加JSON格式文档。

---
+++ Src/NiveumJson/Schema/PositionedTextReader.cs
@@ -0,0 +1,103 @@
+﻿//==========================================================================
+//
+//  File:        PositionedTextReader.cs
+//  Location:    Niveum.Json <Visual C#>
+//  Description: 带位置的文本读取器
+//  Version:     2018.09.17.
+//  Copyright(C) F.R.C.
+//
+//==========================================================================
+
+using System;
+
+namespace Niveum.Json.Syntax
+{
+    public struct TextPosition
+    {
+        public readonly int CharIndex;
+        public readonly int Row;
+        public readonly int Column;
+        public TextPosition(int CharIndex, int Row, int Column)
+        {
+            this.CharIndex = CharIndex;
+            this.Row = Row;
+            this.Column = Column;
+        }
+        public override string ToString()
+        {
+            return $"({Row}, {Column})";
+        }
+    }
+
+    public struct TextRange
+    {
+        public readonly TextPosition Start;
+        public readonly TextPosition End;
+        public TextRange(TextPosition Start, TextPosition End)
+        {
+            this.Start = Start;
+            this.End = End;
+        }
+        public override string ToString()
+        {
+            return Start.ToString() + "-" + End.ToString();
+        }
+    }
+
+    public sealed class PositionedTextReader : IDisposable
+    {
+        public Optional<String> FilePath { get; private set; }
+        public System.IO.TextReader InnerReader { get; private set; }
+        public TextPosition CurrentPosition { get; private set; }
+
+        public PositionedTextReader(Optional<String> FilePath, System.IO.TextReader InnerReader)
+        {
+            this.FilePath = FilePath;
+            this.InnerReader = InnerReader;
+            this.CurrentPosition = new TextPosition(0, 1, 1);
+        }
+
+        public bool EndOfText
+        {
+            get
+            {
+                return InnerReader.Peek() == -1;
+            }
+        }
+        public Char Peek()
+        {
+            var i = InnerReader.Peek();
+            if (i == -1) { throw new InvalidOperationException("EndOfText"); }
+            var c = (Char)(i);
+            return c;
+        }
+        public Char Read()
+        {
+            var i = InnerReader.Read();
+            if (i == -1) { throw new InvalidOperationException("EndOfText"); }
+            var c = (Char)(i);
+            if (c == '\n')
+            {
+                CurrentPosition = new TextPosition(CurrentPosition.CharIndex + 1, CurrentPosition.Row + 1, 1);
+            }
+            else if (c == '\t')
+            {
+                CurrentPosition = new TextPosition(CurrentPosition.CharIndex + 1, CurrentPosition.Row, (CurrentPosition.Column + 3) / 4 * 4 + 1);
+            }
+            else
+            {
+                CurrentPosition = new TextPosition(CurrentPosition.CharIndex + 1, CurrentPosition.Row, CurrentPosition.Column + 1);
+            }
+            return c;
+        }
+
+        public void Dispose()
+        {
+            if (InnerReader != null)
+            {
+                InnerReader.Dispose();
+                InnerReader = null;
+            }
+        }
+    }
+}
---
+++ Src/NiveumJson/Tests/SyntaxTest.cs
@@ -0,0 +1,190 @@
+﻿//==========================================================================
+//
+//  File:        SyntaxTest.cs
+//  Location:    Niveum.Json <Visual C#>
+//  Description: 文法测试
+//  Version:     2018.09.17.
+//  Copyright(C) F.R.C.
+//
+//==========================================================================
+
+using System;
+using System.Collections.Generic;
+using Niveum.Json.Syntax;
+
+namespace Niveum.Json.Tests
+{
+    public sealed class SyntaxTest
+    {
+        public static void DoTest()
+        {
+            Assert(SyntaxEquals(SyntaxParse(@"""123abc\u0020\t\"""""), SyntaxRule.CreateLiteral(TokenLiteral.CreateStringValue("123abc \t\""))));
+
+            Assert(SyntaxEquals(SyntaxParse(@"0"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(0))));
+            Assert(SyntaxEquals(SyntaxParse(@"-1000"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(-1000))));
+            Assert(SyntaxEquals(SyntaxParse(@"0.0"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(0))));
+            Assert(SyntaxEquals(SyntaxParse(@"123.456"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(123.456))));
+            Assert(SyntaxEquals(SyntaxParse(@"-10.1"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(-10.1))));
+            Assert(SyntaxEquals(SyntaxParse(@"100.001e10"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(100.001e10))));
+            Assert(SyntaxEquals(SyntaxParse(@"-100.00e+10"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(-100.00e+10))));
+            Assert(SyntaxEquals(SyntaxParse(@"100.001e-10"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(100.001e-10))));
+            Assert(SyntaxEquals(SyntaxParse(@"1.23456789123456789123456789e10"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(1.23456789123456789123456789e10))));
+            Assert(SyntaxEquals(SyntaxParse(@"1.79769e+308"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(1.79769e+308))));
+            Assert(SyntaxEquals(SyntaxParse(@"-1.79769e+308"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(-1.79769e+308))));
+            Assert(SyntaxEquals(SyntaxParse(@"2.22507e-308"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(2.22507e-308))));
+            Assert(SyntaxEquals(SyntaxParse(@"-2.22507e-308"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNumberValue(-2.22507e-308))));
+
+            Assert(SyntaxEquals(SyntaxParse(@"null"), SyntaxRule.CreateLiteral(TokenLiteral.CreateNullValue())));
+            Assert(SyntaxEquals(SyntaxParse(@"false"), SyntaxRule.CreateLiteral(TokenLiteral.CreateBooleanValue(false))));
+            Assert(SyntaxEquals(SyntaxParse(@"true"), SyntaxRule.CreateLiteral(TokenLiteral.CreateBooleanValue(true))));
+            Assert(SyntaxEquals(SyntaxParse(@"{"), SyntaxRule.CreateLeftBrace()));
+            Assert(SyntaxEquals(SyntaxParse(@"}"), SyntaxRule.CreateRightBrace()));
+            Assert(SyntaxEquals(SyntaxParse(@"["), SyntaxRule.CreateLeftBracket()));
+            Assert(SyntaxEquals(SyntaxParse(@"]"), SyntaxRule.CreateRightBracket()));
+            Assert(SyntaxEquals(SyntaxParse(@":"), SyntaxRule.CreateColon()));
+            Assert(SyntaxEquals(SyntaxParse(@","), SyntaxRule.CreateComma()));
+            Assert(SyntaxEquals(SyntaxParse(" \t\r\n"), SyntaxRule.CreateWhitespace()));
+        }
+
+        private static void Assert(bool b)
+        {
+            if (!b) { throw new InvalidOperationException("Assertion Failed"); }
+        }
+
+        private static SyntaxRule SyntaxParse(String Text)
+        {
+            var TextRanges = new Dictionary<Object, TextRange>();
+            using (var sr = new System.IO.StringReader(Text))
+            using (var ptr = new PositionedTextReader(Optional<String>.Empty, sr))
+            {
+                var t = TokenParser.ReadToken(ptr, TextRanges);
+                if (!ptr.EndOfText) { throw new InvalidOperationException(); }
+                return t;
+            }
+        }
+        private static bool SyntaxEquals(SyntaxRule Left, SyntaxRule Right)
+        {
+            if (Left._Tag != Right._Tag) { return false; }
+            if (Left.OnLiteral)
+            {
+                return SyntaxEquals(Left.Literal, Right.Literal);
+            }
+            else if (Left.OnLeftBracket || Left.OnRightBracket || Left.OnLeftBrace || Left.OnRightBrace || Left.OnColon || Left.OnComma || Left.OnWhitespace)
+            {
+                return true;
+            }
+            else if (Left.OnValue)
+            {
+                return SyntaxEquals(Left.Value, Right.Value);
+            }
+            else if (Left.OnObject)
+            {
+                return SyntaxEquals(Left.Object, Right.Object);
+            }
+            else if (Left.OnMembers)
+            {
+                return SyntaxEquals(Left.Members, Right.Members);
+            }
+            else if (Left.OnArray)
+            {
+                return SyntaxEquals(Left.Array, Right.Array);
+            }
+            else if (Left.OnElements)
+            {
+                return SyntaxEquals(Left.Elements, Right.Elements);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        private static bool SyntaxEquals(TokenLiteral Left, TokenLiteral Right)
+        {
+            if (Left._Tag != Right._Tag) { return false; }
+            if (Left.OnNullValue)
+            {
+                return true;
+            }
+            else if (Left.OnBooleanValue)
+            {
+                return Left.BooleanValue == Right.BooleanValue;
+            }
+            else if (Left.OnNumberValue)
+            {
+                return Left.NumberValue == Right.NumberValue;
+            }
+            else if (Left.OnStringValue)
+            {
+                return Left.StringValue == Right.StringValue;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        private static bool SyntaxEquals(SyntaxValue Left, SyntaxValue Right)
+        {
+            if (Left._Tag != Right._Tag) { return false; }
+            if (Left.OnLiteral)
+            {
+                return SyntaxEquals(Left.Literal, Right.Literal);
+            }
+            else if (Left.OnObject)
+            {
+                return SyntaxEquals(Left.Object, Right.Object);
+            }
+            else if (Left.OnArray)
+            {
+                return SyntaxEquals(Left.Array, Right.Array);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        private static bool SyntaxEquals(SyntaxObject Left, SyntaxObject Right)
+        {
+            if (Left.Members.OnNotHasValue && Right.Members.OnNotHasValue) { return true; }
+            if (Left.Members.OnNotHasValue || Right.Members.OnNotHasValue) { return false; }
+            return SyntaxEquals(Left.Members.Value, Right.Members.Value);
+        }
+        private static bool SyntaxEquals(SyntaxMembers Left, SyntaxMembers Right)
+        {
+            if (Left._Tag != Right._Tag) { return false; }
+            if (Left.OnSingle)
+            {
+                return SyntaxEquals(Left.Single.Item1, Right.Single.Item1) && SyntaxEquals(Left.Single.Item2, Right.Single.Item2);
+            }
+            else if (Left.OnMultiple)
+            {
+                return SyntaxEquals(Left.Multiple.Item1, Right.Multiple.Item1) && SyntaxEquals(Left.Multiple.Item2, Right.Multiple.Item2) && SyntaxEquals(Left.Multiple.Item3, Right.Multiple.Item3);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        private static bool SyntaxEquals(SyntaxArray Left, SyntaxArray Right)
+        {
+            if (Left.Elements.OnNotHasValue && Right.Elements.OnNotHasValue) { return true; }
+            if (Left.Elements.OnNotHasValue || Right.Elements.OnNotHasValue) { return false; }
+            return SyntaxEquals(Left.Elements.Value, Right.Elements.Value);
+        }
+        private static bool SyntaxEquals(SyntaxElements Left, SyntaxElements Right)
+        {
+            if (Left._Tag != Right._Tag) { return false; }
+            if (Left.OnSingle)
+            {
+                return SyntaxEquals(Left.Single, Right.Single);
+            }
+            else if (Left.OnMultiple)
+            {
+                return SyntaxEquals(Left.Multiple.Item1, Right.Multiple.Item1) && SyntaxEquals(Left.Multiple.Item2, Right.Multiple.Item2);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
