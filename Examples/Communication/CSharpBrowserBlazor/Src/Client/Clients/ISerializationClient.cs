9714e64f420f9a7b9bc1b83b3323179efbb3144c
Ianus Inferus <r_ex@163.com>
2018-09-19 15:10:28 +0000

    Example: 增加C# Blazor浏览器通讯示例。

---
+++ Examples/Communication/CSharpBrowserBlazor/Src/Client/Clients/ISerializationClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Client
+{
+    public delegate void BinaryClientEventDelegate(String CommandName, UInt32 CommandHash, Byte[] Parameters);
+    public interface IBinarySerializationClientAdapter
+    {
+        UInt64 Hash { get; }
+        void HandleResult(String CommandName, UInt32 CommandHash, Byte[] Parameters);
+        event BinaryClientEventDelegate ClientEvent;
+    }
+
+    public delegate void JsonClientEventDelegate(String CommandName, UInt32 CommandHash, String Parameters);
+    public interface IJsonSerializationClientAdapter
+    {
+        UInt64 Hash { get; }
+        void HandleResult(String CommandName, UInt32 CommandHash, String Parameters);
+        event JsonClientEventDelegate ClientEvent;
+    }
+}
