7c6ab04c969ccd9134079210a88c5afa2537ff6c
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2019-04-28 04:21:01 +0800

    *: 整理工程和构建脚本。

--- Examples/Expression/CSharp/Src/Build.cmd
+++ Examples/Expression/CSharp/Src/Build.cmd
@@ -1,22 +1,25 @@
 @echo off
 
 setlocal
-if "%SUB_NO_PAUSE_SYMBOL%"=="1" (
-    set NO_PAUSE_SYMBOL=1
-) else (
-    set SUB_NO_PAUSE_SYMBOL=1
-)
-
-for %%p in (Enterprise Professional Community BuildTools) do (
-  if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%p" (
-    set VSDir="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%p"
-  )
-)
-set VSDir=%VSDir:"=%
-echo VSDir=%VSDir%
-
-"%VSDir%\MSBuild\15.0\Bin\MSBuild.exe" /t:Rebuild /p:Configuration=Release
-set /A EXIT_CODE=%ERRORLEVEL%
-
+if "%SUB_NO_PAUSE_SYMBOL%"=="1" set NO_PAUSE_SYMBOL=1
+if /I "%COMSPEC%" == %CMDCMDLINE% set NO_PAUSE_SYMBOL=1
+set SUB_NO_PAUSE_SYMBOL=1
+call :main %*
+set EXIT_CODE=%ERRORLEVEL%
 if not "%NO_PAUSE_SYMBOL%"=="1" pause
 exit /b %EXIT_CODE%
+
+:main
+for %%v in (2019 2017) do (
+  for %%p in (Enterprise Professional Community BuildTools) do (
+    for %%b in (Current 15.0) do (
+      if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe" (
+        set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe"
+        goto MSBuild_Found
+      )
+    )
+  )
+)
+:MSBuild_Found
+
+%MSBuild% /t:Rebuild /p:Configuration=Release || exit /b 1
