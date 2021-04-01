c97a31e4d0052883de00ac4946d94801fb12d70b
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2021-04-01 23:42:17 +0800

    Examples: 将各C++示例中的std::fstream改为使用C API(fopen)等，以获取更详细的错误信息。

--- Examples/Expression/CPP/Src/Streams.h
+++ Examples/Expression/CPP/Src/Streams.h
@@ -1,105 +1,42 @@
 ﻿#pragma once
 
+#include "BaseSystem/StringUtilities.h"
 #include "ExpressionSchema.h"
 #include "ExpressionSchemaBinary.h"
-#include "BaseSystem/StringUtilities.h"
 
 #include <cstdint>
 #include <cstddef>
-#include <stdexcept>
-#include <fstream>
 
 namespace Niveum
 {
     namespace ExpressionSchema
     {
+        class ReadableStreamImpl;
         class ReadableStream : public IReadableStream
         {
         private:
-            std::ifstream s;
-            std::streampos Position;
-            std::streampos FileLength;
+            std::unique_ptr<ReadableStreamImpl> impl_;
         public:
-            ReadableStream(String Path)
-            {
-#if defined WIN32 || defined _WIN32
-                s.open(utf16ToWideChar(Path), std::ifstream::binary);
-#else
-                s.open(utf16ToSystem(Path), std::ifstream::binary);
-#endif
-                if (!s)
-                {
-                    throw std::runtime_error("IOException");
-                }
-                Position = s.tellg();
-                s.seekg(0, std::ios_base::end);
-                FileLength = s.tellg();
-                s.seekg(Position, std::ios_base::beg);
-            }
+            ReadableStream(String Path);
+            virtual ~ReadableStream();
+            void Close();
 
-            uint8_t ReadByte() override
-            {
-                uint8_t b = 0;
-                if (!s.get(reinterpret_cast<char &>(b)))
-                {
-                    throw std::runtime_error("IOException");
-                }
-                Position += 1;
-                return b;
-            }
-            std::vector<std::uint8_t> ReadBytes(std::size_t Size) override
-            {
-                if (Position + static_cast<std::streampos>(Size) > FileLength)
-                {
-                    throw std::runtime_error("IOException");
-                }
-                std::vector<std::uint8_t> l;
-                l.resize(Size);
-                if (!s.read(reinterpret_cast<char *>(l.data()), Size))
-                {
-                    throw std::runtime_error("IOException");
-                }
-                Position += Size;
-                return l;
-            }
+            std::uint8_t ReadByte() override;
+            std::vector<std::uint8_t> ReadBytes(std::size_t Size) override;
         };
 
+        class WritableStreamImpl;
         class WritableStream : public IWritableStream
         {
         private:
-            std::ofstream s;
-            std::streampos Position;
+            std::unique_ptr<WritableStreamImpl> impl_;
         public:
-            WritableStream(String Path)
-            {
-#if defined WIN32 || defined _WIN32
-                s.open(utf16ToWideChar(Path), std::ofstream::binary);
-#else
-                s.open(utf16ToSystem(Path), std::ofstream::binary);
-#endif
-                if (!s)
-                {
-                    throw std::runtime_error("IOException");
-                }
-                Position = s.tellp();
-            }
+            WritableStream(String Path);
+            virtual ~WritableStream();
+            void Close();
 
-            void WriteByte(std::uint8_t b) override
-            {
-                if (!s.put(static_cast<char>(b)))
-                {
-                    throw std::runtime_error("IOException");
-                }
-                Position += 1;
-            }
-            void WriteBytes(const std::vector<std::uint8_t> & l) override
-            {
-                if (!s.write(reinterpret_cast<const char *>(l.data()), l.size()))
-                {
-                    throw std::runtime_error("IOException");
-                }
-                Position += l.size();
-            }
+            void WriteByte(std::uint8_t b) override;
+            void WriteBytes(const std::vector<std::uint8_t> & l) override;
         };
     }
 }
---
+++ Examples/Serialization/CPP/Src/Streams.cpp
@@ -0,0 +1,164 @@
+﻿#if !defined(_CRT_SECURE_NO_WARNINGS) && (defined WIN32 || defined _WIN32)
+#   define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "Streams.h"
+
+#include <stdexcept>
+#include <ios>
+#include <stdio.h>
+#include <string.h>
+
+namespace World
+{
+namespace Streams
+{
+
+class ReadableStreamImpl
+{
+public:
+    String Path;
+    FILE * f{nullptr};
+    std::streampos Position{0};
+    std::streampos FileLength{0};
+};
+
+ReadableStream::ReadableStream(String Path)
+{
+    impl_ = std::make_unique<ReadableStreamImpl>();
+    impl_->Path = Path;
+#if defined WIN32 || defined _WIN32
+    auto err = _wfopen_s(&impl_->f, utf16ToWideChar(Path).c_str(), L"rb");
+    if (err != 0)
+    {
+        throw std::runtime_error(strerror(err) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position = static_cast<std::streampos>(_ftelli64(impl_->f));
+    if (_fseeki64(impl_->f, 0, SEEK_END) != 0)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->FileLength = static_cast<std::streampos>(_ftelli64(impl_->f));
+    if (_fseeki64(impl_->f, 0, SEEK_SET) != 0)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+#else
+    impl_->f = fopen(utf16ToSystem(Path).c_str(), "rb");
+    if (impl_->f == nullptr)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position = static_cast<std::streampos>(ftello(impl_->f));
+    if (fseeko(impl_->f, 0, SEEK_END) != 0)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->FileLength = static_cast<std::streampos>(ftello(impl_->f));
+    if (fseeko(impl_->f, 0, SEEK_SET) != 0)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+#endif
+}
+
+ReadableStream::~ReadableStream()
+{
+    fclose(impl_->f);
+}
+
+void ReadableStream::Close()
+{
+    if (fclose(impl_->f) != 0)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+}
+
+std::uint8_t ReadableStream::ReadByte()
+{
+    std::uint8_t b = 0;
+    if (fread(&b, 1, 1, impl_->f) != 1)
+    {
+        throw std::runtime_error(strerror(ferror(impl_->f)) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position += 1;
+    return b;
+}
+std::vector<std::uint8_t> ReadableStream::ReadBytes(std::size_t Size)
+{
+    if (impl_->Position + static_cast<std::streampos>(Size) > impl_->FileLength)
+    {
+        throw std::runtime_error("EndOfFile" + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    std::vector<std::uint8_t> l;
+    l.resize(Size);
+    if (fread(l.data(), 1, Size, impl_->f) != Size)
+    {
+        throw std::runtime_error(strerror(ferror(impl_->f)) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position += Size;
+    return l;
+}
+
+class WritableStreamImpl
+{
+public:
+    String Path;
+    FILE * f{nullptr};
+    std::streampos Position{0};
+};
+
+WritableStream::WritableStream(String Path)
+{
+    impl_ = std::make_unique<WritableStreamImpl>();
+    impl_->Path = Path;
+#if defined WIN32 || defined _WIN32
+    auto err = _wfopen_s(&impl_->f, utf16ToWideChar(Path).c_str(), L"wb");
+    if (err != 0)
+    {
+        throw std::runtime_error(strerror(err) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position = static_cast<std::streampos>(_ftelli64(impl_->f));
+#else
+    impl_->f = fopen(utf16ToSystem(Path).c_str(), "wb");
+    if (impl_->f == nullptr)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position = static_cast<std::streampos>(ftello(impl_->f));
+#endif
+}
+
+WritableStream::~WritableStream()
+{
+    fclose(impl_->f);
+}
+
+void WritableStream::Close()
+{
+    if (fclose(impl_->f) != 0)
+    {
+        throw std::runtime_error(strerror(errno) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+}
+
+void WritableStream::WriteByte(std::uint8_t b)
+{
+    if (fwrite(&b, 1, 1, impl_->f) != 1)
+    {
+        throw std::runtime_error(strerror(ferror(impl_->f)) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position += 1;
+}
+void WritableStream::WriteBytes(const std::vector<std::uint8_t> & l)
+{
+    if (fwrite(l.data(), 1, l.size(), impl_->f) != l.size())
+    {
+        throw std::runtime_error(strerror(ferror(impl_->f)) + std::string(": ") + utf16ToSystem(impl_->Path));
+    }
+    impl_->Position += l.size();
+}
+
+}
+}
