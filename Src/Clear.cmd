a1a9a6ba2bb09e53cd8d5785d6aefda508099796
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2021-12-18 03:14:01 +0800

    *: 修正清理脚本。

--- Src/Clear.cmd
+++ Src/Clear.cmd
@@ -1,14 +1,8 @@
-@if exist ..\Bin (
-  @pushd ..\Bin
+@if exist ..\Bin\net48 (
+  @pushd ..\Bin\net48
   @for %%a in (*.exe) do @(
     @del %%~na.pdb /F /Q
-    @del %%~na.xml /F /Q
   )
-  @del *.vshost.exe /F /Q
-  @del *.manifest /F /Q
-  @del *.CodeAnalysisLog.xml /F /Q
-  @del *.lastcodeanalysissucceeded /F /Q
-  @del Test.* /F /S /Q
   @popd
 )
 @pause
