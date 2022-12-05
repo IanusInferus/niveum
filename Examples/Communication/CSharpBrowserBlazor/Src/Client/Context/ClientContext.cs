4dbd7443c0b40821bc3bc2c2e46af455752d97c6
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2022-12-05 22:45:33 +0800

    Examples: 优化C#通讯浏览器Blazor示例。

---
+++ Examples/Communication/CSharpBrowserBlazor/Src/Client/Context/ClientContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading;
+using Client;
+using Communication;
+
+public class ClientContext
+{
+    private JsonSerializationClientAdapter jsca;
+    private JsonHttpClient jhc;
+    private IApplicationClient ac;
+    private Timer timer;
+
+    public ClientContext(HttpClient HttpClient, String BaseAddress)
+    {
+        var u = new Uri(BaseAddress);
+        var ApiAddress = $"http://{u.Host}:8003/api/";
+
+        jsca = new JsonSerializationClientAdapter();
+        jhc = new JsonHttpClient(jsca, ApiAddress, "q", false, HttpClient);
+        ac = jsca.GetApplicationClient();
+
+        timer = new Timer(async (object? stateInfo) =>
+        {
+            await ac.ServerTime(new ServerTimeRequest { });
+        }, new AutoResetEvent(false), 20000, 20000);
+    }
+
+    public IApplicationClient ApplicationClient { get { return ac; } }
+}
--- Examples/Communication/CSharpBrowserBlazor/Src/Client/Program.cs
+++ Examples/Communication/CSharpBrowserBlazor/Src/Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var hc = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var cc = new ClientContext(hc, builder.HostEnvironment.BaseAddress);
+builder.Services.AddScoped(sp => hc);
+builder.Services.AddScoped(sp => cc);
+builder.Services.AddScoped(sp => cc.ApplicationClient);
 
 await builder.Build().RunAsync();
