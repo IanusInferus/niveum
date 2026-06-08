99077d3e4b1a7453b854331409955b4667ee3b5c
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2026-06-08 20:15:11 +0800

    Krustallos: 更新Common.cs。

--- Src/Krustallos/Common.cs
+++ Src/Krustallos/Common.cs
@@ -29,43 +29,80 @@ public class TupleAttribute : Firefly.Mapping.MetaSchema.TupleAttribute
 
 public enum OptionalTag
 {
-    NotHasValue = 0,
-    HasValue = 1
+    None = 0,
+    Some = 1
 }
 [TaggedUnion]
 public struct Optional<T>
 {
-    [Tag]
-    public OptionalTag _Tag { get; set; }
+    [Tag] public OptionalTag _Tag { get; set; }
 
-    public Unit NotHasValue { get; set; }
-    public T HasValue { get; set; }
+    private Unit _v_None;
+    private T _v_Some;
 
-    public static Optional<T> CreateNotHasValue() { return new Optional<T> { _Tag = OptionalTag.NotHasValue, NotHasValue = new Unit() }; }
-    public static Optional<T> CreateHasValue(T Value) { return new Optional<T> { _Tag = OptionalTag.HasValue, HasValue = Value }; }
+    public static Optional<T> CreateNone() { return new Optional<T> { _Tag = OptionalTag.None, _v_None = new Unit() }; }
+    public static Optional<T> CreateSome(T Value) { return new Optional<T> { _Tag = OptionalTag.Some, _v_Some = Value }; }
 
-    public Boolean OnNotHasValue { get { return _Tag == OptionalTag.NotHasValue; } }
-    public Boolean OnHasValue { get { return _Tag == OptionalTag.HasValue; } }
+    public Boolean OnNone { get { return _Tag == OptionalTag.None; } }
+    public Boolean OnSome { get { return _Tag == OptionalTag.Some; } }
 
-    public static Optional<T> Empty { get { return CreateNotHasValue(); } }
+    public Unit None
+    {
+        get
+        {
+            if (OnNone)
+            {
+                return (Unit)_v_None;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        set
+        {
+            _v_None = value;
+        }
+    }
+
+    public T Some
+    {
+        get
+        {
+            if (OnSome)
+            {
+                return (T)_v_Some;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        set
+        {
+            _v_Some = value;
+        }
+    }
+
+    public static Optional<T> Empty { get { return CreateNone(); } }
     public static implicit operator Optional<T>(T v)
     {
         if (v == null)
         {
-            return CreateNotHasValue();
+            return CreateNone();
         }
         else
         {
-            return CreateHasValue(v);
+            return CreateSome(v);
         }
     }
     public static explicit operator T(Optional<T> v)
     {
-        if (v.OnNotHasValue)
+        if (v.OnNone)
         {
             throw new InvalidOperationException();
         }
-        return v.HasValue;
+        return v.Some;
     }
     public static Boolean operator ==(Optional<T> Left, Optional<T> Right)
     {
@@ -85,48 +122,77 @@ public struct Optional<T>
     }
     public override Boolean Equals(Object obj)
     {
-        return Equals(this, obj);
+        if (obj == null) { return Equals(this, null); }
+        if (obj.GetType() != typeof(Optional<T>)) { return false; }
+        var o = (Optional<T>)(obj);
+        return Equals(this, o);
     }
     public override Int32 GetHashCode()
     {
-        if (OnNotHasValue) { return 0; }
-        return HasValue.GetHashCode();
+        if (OnNone) { return 0; }
+        return Some.GetHashCode();
     }
 
     private static Boolean Equals(Optional<T> Left, Optional<T> Right)
     {
-        if (Left.OnNotHasValue && Right.OnNotHasValue)
+        if (Left.OnNone && Right.OnNone)
         {
             return true;
         }
-        if (Left.OnNotHasValue || Right.OnNotHasValue)
+        if (Left.OnNone || Right.OnNone)
         {
             return false;
         }
-        return Left.HasValue.Equals(Right.HasValue);
+        return Left.Some.Equals(Right.Some);
     }
     private static Boolean Equals(Optional<T>? Left, Optional<T>? Right)
     {
-        if ((!Left.HasValue || Left.Value.OnNotHasValue) && (!Right.HasValue || Right.Value.OnNotHasValue))
+        if ((!Left.HasValue || Left.Value.OnNone) && (!Right.HasValue || Right.Value.OnNone))
         {
             return true;
         }
-        if (!Left.HasValue || Left.Value.OnNotHasValue || !Right.HasValue || Right.Value.OnNotHasValue)
+        if (!Left.HasValue || Left.Value.OnNone || !Right.HasValue || Right.Value.OnNone)
         {
             return false;
         }
         return Equals(Left.Value, Right.Value);
     }
 
+    public T Value
+    {
+        get
+        {
+            if (OnSome)
+            {
+                return Some;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
     public T ValueOrDefault(T Default)
     {
-        if (OnHasValue)
+        if (OnSome)
         {
-            return HasValue;
+            return Some;
         }
         else
         {
             return Default;
+        }
+    }
+
+    public override String ToString()
+    {
+        if (OnSome)
+        {
+            return Some.ToString();
+        }
+        else
+        {
+            return "-";
         }
     }
 }
